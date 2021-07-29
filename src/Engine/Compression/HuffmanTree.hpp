bb5f4a58a5187131596aba6ff7761d0661a9b7a1
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2021-07-29 15:33:52 +0200

    Unified static functions with HuffmanTree

--- src/Engine/Compression/HuffmanTree.hpp
+++ src/Engine/Compression/HuffmanTree.hpp
@@ -5,11 +5,13 @@
 #include "../Utility/Concepts.hpp"
 
 #include <unordered_map>
+#include <climits>
+#include <bitset>
 #include <map>
 
 namespace ge {
 
-    template <typename CharType = uint8_t, SizeType FrequencyType = std::size_t, CharType InnerNode = '$'>
+    template <typename CharType = uint8_t, SizeType FrequencyType = std::size_t>
     class HuffmanTree {
     public:
         typedef std::unordered_map<CharType, FrequencyType>     DecodingMap;
@@ -17,13 +19,13 @@ namespace ge {
         typedef std::map<CharType, std::string>                 CodesMap;
         typedef std::vector<CharType>                           FrequencyArray;
 
+        explicit HuffmanTree(const FrequencyArray& frequency);
         explicit HuffmanTree(const CharactersMap& data);
 
         CodesMap getCoding(void) const noexcept;
         DecodingMap getDecoder(void) const noexcept;
 
-        static DecodingMap createDecodingMap(const FrequencyArray& frequency);
-        static DecodingMap createDefaultDecoder(void);
+        static HuffmanTree createDeflateDecoder(void) requires (sizeof(CharType) * CHAR_BIT > 8);
 
     private:
         struct Node {
@@ -31,35 +33,36 @@ namespace ge {
             std::unique_ptr<Node> rightNode;
             FrequencyType frequency;
             CharType character;
+            bool isInner;
 
-            explicit Node(CharType character = 0, const FrequencyType& frequency = 0)
+            explicit Node(CharType character = 0, const FrequencyType& frequency = 0, bool isInner = false)
                 : leftNode{nullptr}, rightNode{nullptr}, frequency{frequency},
-                character{character} {}
+                character{character}, isInner{isInner} {}
             explicit Node(const std::pair<CharType, FrequencyType>& frequencyPair)
                 : leftNode{nullptr}, rightNode{nullptr}, frequency{frequencyPair.second},
-                character{frequencyPair.first} {}
+                character{frequencyPair.first}, isInner{false} {}
 
             friend bool operator> (const Node& left, const Node& right) noexcept { return left.frequency > right.frequency; }
             friend bool operator< (const Node& left, const Node& right) noexcept { return left.frequency < right.frequency; }
         };
 
-        typedef std::unique_ptr<Node>                            NodePtr;
-        typedef std::reference_wrapper<const NodePtr>            NodeCRef;
+        typedef std::unique_ptr<Node>                                   NodePtr;
+        typedef std::reference_wrapper<const NodePtr>                   NodeCRef;
 
-        typedef std::array<FrequencyType, sizeof(FrequencyType)> CountedArray;
-        typedef std::unordered_map<CharType, FrequencyType>      FrequencyMap;
+        typedef std::array<FrequencyType, sizeof(CharType) * CHAR_BIT>  CountedArray;
 
         void walkThrough(NodeCRef node, CodesMap& map, std::string code) const;
         void walkThrough(NodeCRef node, DecodingMap& map, FrequencyType code) const;
+        void addNode(CharType length, CharType character, FrequencyType code);
 
         NodePtr root;
 
-        static CountedArray generateFrequencyArray(const FrequencyArray& frequency);
-        static CountedArray generateSmallestCodes(const CountedArray& counted, FrequencyType min, FrequencyType max);
+        CountedArray generateFrequencyArray(const FrequencyArray& frequency);
+        CountedArray generateSmallestCodes(const CountedArray& counted, FrequencyType min, FrequencyType max);
     };
 
-    template <typename CharType, SizeType FrequencyType, CharType InnerNode>
-    HuffmanTree<CharType, FrequencyType, InnerNode>::HuffmanTree(const CharactersMap& data) : root{nullptr} {
+    template <typename CharType, SizeType FrequencyType>
+    HuffmanTree<CharType, FrequencyType>::HuffmanTree(const CharactersMap& data) : root{nullptr} {
         if (!data.size())
             throw HuffmanTreeEmptyMapException{};
         MinHeap<NodePtr> heap;
@@ -68,7 +71,7 @@ namespace ge {
         while (heap.size() != 1) {
             auto left = heap.popBack();
             auto right = heap.popBack();
-            auto node = std::make_unique<Node>(InnerNode, left->frequency + right->frequency);
+            auto node = std::make_unique<Node>(0, left->frequency + right->frequency, true);
             node->leftNode = std::move(left);
             node->rightNode = std::move(right);
             heap.push(std::move(node));
@@ -76,21 +79,46 @@ namespace ge {
         root = std::move(heap.popBack());
     }
 
-    template <typename CharType, SizeType FrequencyType, CharType InnerNode>
-    HuffmanTree<CharType, FrequencyType, InnerNode>::CountedArray
-        HuffmanTree<CharType, FrequencyType, InnerNode>::generateFrequencyArray(const FrequencyArray& frequency)
+    template <typename CharType, SizeType FrequencyType>
+    HuffmanTree<CharType, FrequencyType>::HuffmanTree(const FrequencyArray& frequency) : root{nullptr} {
+        auto [minFreq, maxFreq] = std::ranges::minmax_element(frequency);
+        auto smallestCodes = generateSmallestCodes(generateFrequencyArray(frequency), *minFreq, *maxFreq);
+        std::ranges::for_each(frequency, [&, i = 0](const auto& bits) mutable -> void {
+            if (auto iter = i++; bits) {
+                addNode(bits, iter, smallestCodes[bits]);
+                ++smallestCodes[bits];
+            }
+        });
+    }
+
+    template <typename CharType, SizeType FrequencyType>
+    void HuffmanTree<CharType, FrequencyType>::addNode(CharType length, CharType character, FrequencyType code) {
+        auto node = std::make_unique<Node>(character, length);
+        std::reference_wrapper<NodePtr> actualNode {root};
+        std::bitset<sizeof(FrequencyType) * CHAR_BIT> bits {code};
+        while (length--) {
+            if (!actualNode.get())
+                actualNode.get() = std::move(std::make_unique<Node>(0, 0, true));
+            actualNode = std::ref(bits[length] ? actualNode.get()->rightNode : actualNode.get()->leftNode);
+        };
+        actualNode.get() = std::move(node);
+    }
+
+    template <typename CharType, SizeType FrequencyType>
+    HuffmanTree<CharType, FrequencyType>::CountedArray
+        HuffmanTree<CharType, FrequencyType>::generateFrequencyArray(const FrequencyArray& frequency)
     {
         CountedArray counted;
         counted.fill(0);
         for (const auto& length : frequency)
-            counted[length]++;
+            ++counted[length];
         counted[0] = 0;
         return counted;
     }
 
-    template <typename CharType, SizeType FrequencyType, CharType InnerNode>
-    HuffmanTree<CharType, FrequencyType, InnerNode>::CountedArray
-        HuffmanTree<CharType, FrequencyType, InnerNode>::generateSmallestCodes(const CountedArray& counted, FrequencyType min, FrequencyType max)
+    template <typename CharType, SizeType FrequencyType>
+    HuffmanTree<CharType, FrequencyType>::CountedArray
+        HuffmanTree<CharType, FrequencyType>::generateSmallestCodes(const CountedArray& counted, FrequencyType min, FrequencyType max)
     {
         CountedArray smallestCodes;
         FrequencyType code = 0;
@@ -99,25 +127,9 @@ namespace ge {
         return smallestCodes;
     }
 
-    template <typename CharType, SizeType FrequencyType, CharType InnerNode>
-    HuffmanTree<CharType, FrequencyType, InnerNode>::DecodingMap
-        HuffmanTree<CharType, FrequencyType, InnerNode>::createDecodingMap(const FrequencyArray& frequency)
-    {
-        auto [minFreq, maxFreq] = std::ranges::minmax_element(frequency);
-        CountedArray smallestCodes = generateSmallestCodes(generateFrequencyArray(frequency), *minFreq, *maxFreq);
-        DecodingMap decoder;
-        std::ranges::for_each(frequency, [&, i = 0](const auto& bits) mutable -> void {
-            if (auto iter = i++; bits) {
-                decoder[iter] = smallestCodes[bits];
-                ++smallestCodes[bits];
-            }
-        });
-        return decoder;
-    }
-
-    template <typename CharType, SizeType FrequencyType, CharType InnerNode>
-    HuffmanTree<CharType, FrequencyType, InnerNode>::DecodingMap
-        HuffmanTree<CharType, FrequencyType, InnerNode>::createDefaultDecoder(void)
+    template <typename CharType, SizeType FrequencyType>
+    HuffmanTree<CharType, FrequencyType>
+        HuffmanTree<CharType, FrequencyType>::createDeflateDecoder(void) requires (sizeof(CharType) * CHAR_BIT > 8)
     {
         FrequencyArray frequency;
         frequency.reserve(288);
@@ -125,45 +137,45 @@ namespace ge {
         std::ranges::fill_n(std::back_inserter(frequency), 112, 9);
         std::ranges::fill_n(std::back_inserter(frequency), 24, 7);
         std::ranges::fill_n(std::back_inserter(frequency), 8, 8);
-        return createDecodingMap(frequency);
+        return HuffmanTree<CharType, FrequencyType>{frequency};
     }
 
-    template <typename CharType, SizeType FrequencyType, CharType InnerNode>
-    void HuffmanTree<CharType, FrequencyType, InnerNode>::walkThrough(NodeCRef node, CodesMap& map, std::string code) const {
-        while (node.get()) {
-            if (node.get()->character != InnerNode)
-                map[node.get()->character] = code;
+    template <typename CharType, SizeType FrequencyType>
+    void HuffmanTree<CharType, FrequencyType>::walkThrough(NodeCRef node, CodesMap& map, std::string code) const {
+        if (!node.get()) return;
+        while (node.get()->isInner) {
             walkThrough(std::cref(node.get()->leftNode), map, code + "0");
             node = std::cref(node.get()->rightNode);
             code += "1";
         }
+        map[node.get()->character] = code;
     }
 
-    template <typename CharType, SizeType FrequencyType, CharType InnerNode>
-        HuffmanTree<CharType, FrequencyType, InnerNode>::CodesMap
-    HuffmanTree<CharType, FrequencyType, InnerNode>::getCoding(void) const noexcept {
+    template <typename CharType, SizeType FrequencyType>
+        HuffmanTree<CharType, FrequencyType>::CodesMap
+    HuffmanTree<CharType, FrequencyType>::getCoding(void) const noexcept {
         CodesMap codesMap;
         walkThrough(root, codesMap, "");
         return codesMap;
     }
 
-    template <typename CharType, SizeType FrequencyType, CharType InnerNode>
-        HuffmanTree<CharType, FrequencyType, InnerNode>::DecodingMap
-    HuffmanTree<CharType, FrequencyType, InnerNode>::getDecoder(void) const noexcept {
+    template <typename CharType, SizeType FrequencyType>
+        HuffmanTree<CharType, FrequencyType>::DecodingMap
+    HuffmanTree<CharType, FrequencyType>::getDecoder(void) const noexcept {
         DecodingMap codesMap;
         walkThrough(root, codesMap, 0);
         return codesMap;
     }
 
-    template <typename CharType, SizeType FrequencyType, CharType InnerNode>
-    void HuffmanTree<CharType, FrequencyType, InnerNode>::walkThrough(NodeCRef node, DecodingMap& map, FrequencyType code) const {
-        while (node.get()) {
-            if (node.get()->character != InnerNode)
-                map[node.get()->character] = code;
-            walkThrough(std::cref(node.get()->leftNode), map, 2 * code);
+    template <typename CharType, SizeType FrequencyType>
+    void HuffmanTree<CharType, FrequencyType>::walkThrough(NodeCRef node, DecodingMap& map, FrequencyType code) const {
+        if (!node.get()) return;
+        while (node.get()->isInner) {
+            walkThrough(std::cref(node.get()->leftNode), map, code << 1);
             node = std::cref(node.get()->rightNode);
-            code = 2 * code + 1;
+            code = (code << 1) + 1;
         }
+        map[node.get()->character] = code;
     }
 
 
