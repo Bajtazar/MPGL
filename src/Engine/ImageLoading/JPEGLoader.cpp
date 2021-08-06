7976eb9ea775a57376371be280eede090f397c79
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2021-08-06 21:41:28 +0200

    Added DQT and SOF0 chunks parsing to JPEGLoader

--- src/Engine/ImageLoading/JPEGLoader.cpp
+++ src/Engine/ImageLoading/JPEGLoader.cpp
@@ -3,6 +3,7 @@
 #include "../Exceptions/ImageLoadingFileCorruptionException.hpp"
 #include "../Exceptions/ImageLoadingInvalidTypeException.hpp"
 #include "../Exceptions/ImageLoadingFileOpenException.hpp"
+#include "../Exceptions/NotSupportedException.hpp"
 #include "../Utility/FunctionalWrapper.hpp"
 #include "../Utility/Ranges.hpp"
 
@@ -14,7 +15,7 @@ namespace ge {
 
     const std::string JPEGLoader::Tag{"jpeg"};
 
-    JPEGLoader::JPEGLoader(const std::string& fileName) : LoaderInterface{std::move(fileName)}, huffmanTrees{0} {
+    JPEGLoader::JPEGLoader(const std::string& fileName) : LoaderInterface{std::move(fileName)} {
         std::ifstream file{this->fileName.c_str(), std::ios::binary};
         if (!file.good() || !file.is_open())
             throw ImageLoadingFileOpenException{this->fileName};
@@ -38,14 +39,41 @@ namespace ge {
 
     void JPEGLoader::DHTChunk::operator() (std::istream& data) {
         uint16_t length = readType<uint16_t, true>(data) - 19;
-        uint8_t ht = readType<uint8_t>(data); // parse
-        std::array<uint8_t, 16> symbolsLengths;
-        std::ranges::for_each(symbolsLengths, [&data](auto& symbol) { data.get(reinterpret_cast<char&>(symbol)); });
+        uint8_t header = readType<uint8_t>(data); // parse
+        std::array<uint8_t, 17> symbolsLengths{};
+        std::ranges::for_each(symbolsLengths | std::views::drop(1), [&data](auto& symbol)
+            { data.get(reinterpret_cast<char&>(symbol)); });
         if (ge::accumulate(symbolsLengths, 0u) != length)
             throw ImageLoadingFileCorruptionException{loader.fileName};
         std::vector<uint8_t> characters(length, 0);
         std::ranges::for_each(characters, [&data](auto& symbol){ data.get(reinterpret_cast<char&>(symbol)); });
-        HuffmanTree<> tree{symbolsLengths, characters}; //// debug this
+        loader.huffmanTables.emplace_back(HuffmanTree<uint16_t>{symbolsLengths, characters}, header, loader.fileName);
+    }
+
+    void JPEGLoader::DQTChunk::operator() (std::istream& data) {
+        uint16_t length = readType<uint16_t, true>(data) - 3;
+        uint8_t header = readType<uint8_t>(data);
+        QuantizationTable table;
+        table.number = 0xF & header;
+        table.precision = 0xF0 & header;
+        table.information.resize(length);
+        std::ranges::for_each(table.information, [&data](uint8_t& quant)
+            { data.get(reinterpret_cast<char&>(quant)); });
+        loader.quantizationTables.push_back(std::move(table));
+    }
+
+    void JPEGLoader::SOF0Chunk::operator() (std::istream& data) {
+        uint16_t length = readType<uint16_t, true>(data) - 8;
+        if (readType<uint8_t>(data) != 8)
+            throw NotSupportedException{"Other JPEG data precisions than 8 are not supported."};
+        uint16_t height = readType<uint16_t, true>(data);
+        loader.pixels.resize(height, readType<uint16_t, true>(data));
+        uint8_t components = readType<uint8_t>(data);
+        if (length / 3 != components)
+            throw ImageLoadingFileCorruptionException{loader.fileName};
+        for (uint8_t i = 0;i != components; ++i)
+            loader.componentsTable.emplace_back(readType<uint8_t>(data),
+                readType<uint8_t>(data), readType<uint8_t>(data));
     }
 
     void JPEGLoader::StartOfScanChunk::operator() (std::istream& data) {
@@ -61,8 +89,21 @@ namespace ge {
         }
     }
 
+    JPEGLoader::HuffmanTable::HuffmanTable(HuffmanTree<uint16_t> tree, uint8_t header, const std::string& fileName)
+        : decoder{std::move(tree)}, number{static_cast<uint8_t>(0xF & header)}, type{static_cast<bool>(0x10 & header)}
+    {
+        if (0xe0 & header)
+            throw ImageLoadingFileCorruptionException{fileName};
+    }
+
+    JPEGLoader::Component::Component(uint8_t id, uint8_t samplings, uint8_t tableNumber) noexcept
+        : id{id}, verticalSampling{static_cast<uint8_t>(0xF & samplings)},
+            horizontalSampling{static_cast<uint8_t>(0xF0 & samplings)},tableNumber{tableNumber} {}
+
     const std::map<uint16_t, std::function<std::unique_ptr<JPEGLoader::ChunkInterface> (JPEGLoader&)>> JPEGLoader::chunkParser {
         {0xFFC4, FunctionalWrapper<JPEGLoader::DHTChunk, JPEGLoader::ChunkInterface>{}},
+        {0xFFDB, FunctionalWrapper<JPEGLoader::DQTChunk, JPEGLoader::ChunkInterface>{}},
+        {0xFFC0, FunctionalWrapper<JPEGLoader::SOF0Chunk, JPEGLoader::ChunkInterface>{}},
         {0xFFDA, FunctionalWrapper<JPEGLoader::StartOfScanChunk, JPEGLoader::ChunkInterface>{}}
     };
 
--- src/Engine/ImageLoading/JPEGLoader.hpp
+++ src/Engine/ImageLoading/JPEGLoader.hpp
@@ -5,6 +5,7 @@
 
 #include <functional>
 #include <memory>
+#include <tuple>
 #include <map>
 
 namespace ge {
@@ -17,7 +18,6 @@ namespace ge {
 
         ~JPEGLoader(void) noexcept = default;
     private:
-
         class ChunkInterface {
         public:
             explicit ChunkInterface(JPEGLoader& loader) noexcept : loader{loader} {}
@@ -27,22 +27,57 @@ namespace ge {
             JPEGLoader& loader;
         };
 
-        struct StartOfScanChunk : public ChunkInterface {
-            explicit StartOfScanChunk(JPEGLoader& loader) noexcept : ChunkInterface{loader} {}
-            virtual void operator() (std::istream& data) final;
-            ~StartOfScanChunk(void) noexcept = default;
-        };
-
         struct DHTChunk : public ChunkInterface {
             explicit DHTChunk(JPEGLoader& loader) noexcept : ChunkInterface{loader} {}
             virtual void operator() (std::istream& data) final;
             ~DHTChunk(void) noexcept = default;
         };
 
+        struct DQTChunk : public ChunkInterface {
+            explicit DQTChunk(JPEGLoader& loader) noexcept : ChunkInterface{loader} {}
+            virtual void operator() (std::istream& data) final;
+            ~DQTChunk(void) noexcept = default;
+        };
+
+        struct SOF0Chunk : public ChunkInterface {
+            explicit SOF0Chunk(JPEGLoader& loader) noexcept : ChunkInterface{loader} {}
+            virtual void operator() (std::istream& data) final;
+            ~SOF0Chunk(void) noexcept = default;
+        };
+
+        struct StartOfScanChunk : public ChunkInterface {
+            explicit StartOfScanChunk(JPEGLoader& loader) noexcept : ChunkInterface{loader} {}
+            virtual void operator() (std::istream& data) final;
+            ~StartOfScanChunk(void) noexcept = default;
+        };
+
+        struct HuffmanTable {
+            HuffmanTree<uint16_t>::Decoder decoder;
+            uint8_t number : 4;
+            bool type : 1;
+            explicit HuffmanTable(HuffmanTree<uint16_t> tree, uint8_t header, const std::string& fileName);
+        };
+
+        struct QuantizationTable {
+            std::vector<uint8_t> information;
+            uint8_t number : 4;
+            uint8_t precision : 4;
+        };
+
+        struct Component {
+            uint8_t id;
+            uint8_t verticalSampling : 4;
+            uint8_t horizontalSampling: 4;
+            uint8_t tableNumber;
+            explicit Component(uint8_t id, uint8_t samplings, uint8_t tableNumber) noexcept;
+        };
+
         void parseChunks(std::ifstream& file);
         void invokeParser(uint16_t signature, std::istream& file);
 
-        uint8_t huffmanTrees : 2;
+        std::vector<HuffmanTable> huffmanTables;
+        std::vector<QuantizationTable> quantizationTables;
+        std::vector<Component> componentsTable;
 
         static const std::map<uint16_t, std::function<std::unique_ptr<ChunkInterface> (JPEGLoader&)>> chunkParser;
     };
