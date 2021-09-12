9e6e074c19fc3d09701ddc9c2129f02232b9f3f5
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2021-09-12 14:07:45 +0200

    Actualized Glyph class

--- src/Engine/Core/Text/Glyph.hpp
+++ src/Engine/Core/Text/Glyph.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <inttypes.h>
-
 #include "../Texture.hpp"
 #include "../../Mathematics/Vector.hpp"
 
@@ -10,27 +8,31 @@ namespace ge {
     template <Allocator Alloc = Texture<>::allocator_type>
     struct Glyph {
         Texture<Alloc>      texture;
-        Vector2i            dimmensions;
+        Vector2ui           dimmensions;
         Vector2i            bearing;
         uint32_t            advance;
 
-        // constructor
+        explicit Glyph(Texture<Alloc> const& texture,
+            Vector2ui const& dimmensions, Vector2i const& bearing,
+            uint32_t advance) noexcept : texture{texture},
+                dimmensions{dimmensions}, bearing{bearing},
+                advance{advance} {}
 
         template <std::size_t Index>
             requires (Index < 4)
-        inline constexpr auto&& get(void) & noexcept { return getHelper<Index>(*this); }
+        inline constexpr auto&& get(void) & noexcept { return helper<Index>(*this); }
 
         template <std::size_t Index>
             requires (Index < 4)
-        inline constexpr auto&& get(void) && noexcept { return getHelper<Index>(*this); }
+        inline constexpr auto&& get(void) && noexcept { return helper<Index>(*this); }
 
         template <std::size_t Index>
             requires (Index < 4)
-        inline constexpr auto&& get(void) const& noexcept { return getHelper<Index>(*this); }
+        inline constexpr auto&& get(void) const& noexcept { return helper<Index>(*this); }
 
     private:
         template <std::size_t Index, class Base>
-        inline constexpr auto&& getHelper(Base&& base) const noexcept {
+        inline constexpr auto&& helper(Base&& base) const noexcept {
             if constexpr (Index == 0)
                 return std::forward<Base>(base).texture;
             else if constexpr (Index == 1)
@@ -51,11 +53,13 @@ namespace std {
     struct tuple_size<ge::Glyph<Alloc>> : integral_constant<size_t, 4> {};
 
     template <ge::Allocator Alloc>
-    struct tuple_element<0, ge::Glyph<Alloc>> { using type = Texture<Alloc>; };
+    struct tuple_element<0, ge::Glyph<Alloc>> { using type = ge::Texture<Alloc>; };
 
-    template <ge::Allocator Alloc, size_t Index>
-        requires (Index > 0 && Index < 3)
-    struct tuple_element<Index, ge::Glyph<Alloc>> { using type = Vector2i; };
+    template <ge::Allocator Alloc>
+    struct tuple_element<1, ge::Glyph<Alloc>> { using type = ge::Vector2ui; };
+
+    template <ge::Allocator Alloc>
+    struct tuple_element<2, ge::Glyph<Alloc>> { using type = ge::Vector2i; };
 
     template <ge::Allocator Alloc>
     struct tuple_element<3, ge::Glyph<Alloc>> { using type = uint32_t; };
