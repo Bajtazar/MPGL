fa264d002bda624f685ded61757addb29da9ce28
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2021-09-16 00:50:05 +0200

    Improved RenderWindow implementation

--- src/Engine/Core/RenderWindow.cpp
+++ src/Engine/Core/RenderWindow.cpp
@@ -6,39 +6,47 @@ namespace ge {
 
     using std::operator""us;
 
-    RenderWindow::RenderWindow(const Vector2ui& dimmensions, std::string title, Options options, GLFWmonitor* monitor, GLFWwindow* share) noexcept(false) : WindowInterface{dimmensions, title, options, monitor, share}, sleepTime{0us}, lastTime{0us} {
+    RenderWindow::RenderWindow(Vector2ui const& dimmensions,
+        std::string const& title, Options options, GLFWmonitor* monitor,
+        GLFWwindow* share)
+        : WindowInterface{dimmensions, title, options, monitor, share},
+        sleepTime{0us}, lastTime{0us}
+    {
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
     }
 
-    void RenderWindow::pushDrawable(const std::shared_ptr<Drawable>& drawable) noexcept {
+    void RenderWindow::pushDrawable(DrawablePtr const& drawable) noexcept {
         if (auto ptr = std::dynamic_pointer_cast<Transformable>(drawable))
             transformables.push_back(ptr);
         drawables.push_back(drawable);
     }
 
-    void RenderWindow::pushDrawable(std::shared_ptr<Drawable>&& drawable) noexcept {
+    void RenderWindow::pushDrawable(DrawablePtr&& drawable) noexcept {
         if (auto ptr = std::dynamic_pointer_cast<Transformable>(drawable))
             transformables.push_back(ptr);
         drawables.push_back(std::move(drawable));
     }
 
     void RenderWindow::setDrawablesShaders(void) noexcept {
-        std::ranges::for_each(drawables, [this](auto& drawable){ drawable->setShaders(this->shaders); });
+        std::ranges::for_each(drawables, [this](auto& drawable)
+            { drawable->setShaders(this->shaders); });
     }
 
     void RenderWindow::copyDrawablesToGPU(void) const noexcept {
-        std::ranges::for_each(drawables, [](auto const& drawable){ drawable->copyToGPU(); });
+        std::ranges::for_each(drawables, [](auto const& drawable)
+            { drawable->copyToGPU(); });
     }
 
     void RenderWindow::drawDrawables(void) const noexcept {
-        std::ranges::for_each(drawables, [](const auto& drawable){ drawable->draw(); });
+        std::ranges::for_each(drawables, [](const auto& drawable)
+            { drawable->draw(); });
     }
 
     void RenderWindow::clear(const Color& color) noexcept{
         WindowInterface::clear(color);
-        lastTime = std::chrono::steady_clock::now();
+        lastTime = ThreadClock::now();
     }
 
     void RenderWindow::draw(void) noexcept {
@@ -57,7 +65,7 @@ namespace ge {
     int32_t RenderWindow::windowLoop(const Color& background) noexcept {
         setDrawablesShaders();
         copyDrawablesToGPU();
-        while (!(*this)) {
+        while (!shouldWindowClose()) {
             clear(background);
             drawDrawables();
             draw();
--- src/Engine/Core/RenderWindow.hpp
+++ src/Engine/Core/RenderWindow.hpp
@@ -7,76 +7,100 @@
 #include "Figures/ShaderLibrary.hpp"
 #include "Registers/TickRegister.hpp"
 
-#include <chrono>
-
 namespace ge {
 
     class RenderWindow : virtual private WindowInterface {
     public:
-        explicit RenderWindow(const Vector2ui& dimmensions, std::string title, Options options = Options(), GLFWmonitor* monitor = nullptr, GLFWwindow* share = nullptr) noexcept(false);
-        RenderWindow(const RenderWindow& window) noexcept = delete;
+        typedef std::shared_ptr<Drawable>           DrawablePtr;
+        typedef std::shared_ptr<Vector2ui>          ScenePtr;
+        typedef std::vector<DrawablePtr>            Drawables;
+
+        explicit RenderWindow(Vector2ui const& dimmensions,
+            std::string const& title, Options options = Options(),
+            GLFWmonitor* monitor = nullptr, GLFWwindow* share = nullptr);
+
+        RenderWindow(RenderWindow const& window) noexcept = delete;
         RenderWindow(RenderWindow&& window) noexcept = delete;
 
         RenderWindow& operator= (const RenderWindow& window) noexcept = delete;
         RenderWindow& operator= (RenderWindow&& window) noexcept = delete;
 
-        operator bool() const noexcept { return shouldWindowClose(); }
+        ScenePtr const& getWindowDimmensions(void) const noexcept
+            { return WindowInterface::getWindowDimmensions(); }
+        std::string const& getWindowTitle(void) const noexcept
+            { return WindowInterface::getWindowTitle(); }
 
-        const std::shared_ptr<Vector2ui>& getWindowDimmensions(void) const noexcept { return WindowInterface::getWindowDimmensions(); }
-        const std::string& getWindowTitle(void) const noexcept { return WindowInterface::getWindowTitle(); }
+        void pushDrawable(DrawablePtr const& drawable) noexcept;
+        void pushDrawable(DrawablePtr&& drawable) noexcept;
 
-        void pushDrawable(const std::shared_ptr<Drawable>& drawable) noexcept;
-        void pushDrawable(std::shared_ptr<Drawable>&& drawable) noexcept;
         template <std::derived_from<Drawable> T, typename... Args>
-            requires std::is_constructible_v<T, const std::shared_ptr<Vector2ui>&, Args...>
+            requires std::is_constructible_v<T, ScenePtr const&, Args...>
         void emplaceDrawable(Args&&... args) noexcept;
+
+        bool setFPSLimit(std::size_t fpsLimit) noexcept;
+
+        DrawablePtr const& operator[] (std::size_t index) const noexcept
+            { return drawables[index]; }
+        DrawablePtr& operator[] (std::size_t index) noexcept
+            { return drawables[index]; }
+
+        using iterator = typename Drawables::iterator;
+        using const_iterator = typename Drawables::const_iterator;
+        using reverse_iterator = typename Drawables::reverse_iterator;
+        using const_reverse_iterator = typename Drawables::const_reverse_iterator;
+
+        iterator begin(void) noexcept
+            { return drawables.begin(); }
+        iterator end(void) noexcept
+            { return drawables.end(); }
+        const_iterator begin(void) const noexcept
+            { return drawables.begin(); }
+        const_iterator end(void) const noexcept
+            { return drawables.end(); }
+
+        const_iterator cbegin(void) const noexcept
+            { return drawables.cbegin(); }
+        const_iterator cend(void) const noexcept
+            { return drawables.cend(); }
+
+        reverse_iterator rbegin(void) noexcept
+            { return drawables.rbegin(); }
+        reverse_iterator rend(void) noexcept
+            { return drawables.rend(); }
+
+        const_reverse_iterator crbegin(void) const noexcept
+            { return drawables.crbegin(); }
+        const_reverse_iterator crend(void) const noexcept
+            { return drawables.crend(); }
+
+        int32_t windowLoop(Color const& background = Color::literals::Black) noexcept;
+
+        ShaderLibrary& getShaderLib(void) noexcept
+            { return shaders; }
+
+        ~RenderWindow(void) noexcept = default;
+    private:
+        typedef std::chrono::microseconds           Duration;
+        typedef std::chrono::steady_clock           ThreadClock;
+        typedef ThreadClock::time_point             TimePoint;
 
         void setDrawablesShaders(void) noexcept;
         void copyDrawablesToGPU(void) const noexcept;
         void drawDrawables(void) const noexcept;
-
-        bool setFPSLimit(std::size_t fpsLimit) noexcept;
-
-        const std::shared_ptr<Drawable>& operator[] (std::size_t index) const noexcept { return drawables[index]; }
-        std::shared_ptr<Drawable>& operator[] (std::size_t index) noexcept { return drawables[index]; }
-
-        using iterator = std::vector<std::shared_ptr<Drawable>>::iterator;
-        using const_iterator = std::vector<std::shared_ptr<Drawable>>::const_iterator;
-        using reverse_iterator = std::vector<std::shared_ptr<Drawable>>::reverse_iterator;
-        using const_reverse_iterator = std::vector<std::shared_ptr<Drawable>>::const_reverse_iterator;
-
-        iterator begin(void) noexcept { return drawables.begin(); }
-        iterator end(void) noexcept { return drawables.end(); }
-        const_iterator begin(void) const noexcept { return drawables.begin(); }
-        const_iterator end(void) const noexcept { return drawables.end(); }
-
-        const_iterator cbegin(void) const noexcept { return drawables.cbegin(); }
-        const_iterator cend(void) const noexcept { return drawables.cend(); }
-
-        reverse_iterator rbegin(void) noexcept { return drawables.rbegin(); }
-        reverse_iterator rend(void) noexcept { return drawables.rend(); }
-
-        const_reverse_iterator crbegin(void) const noexcept { return drawables.crbegin(); }
-        const_reverse_iterator crend(void) const noexcept { return drawables.crend(); }
-
-        int32_t windowLoop(const Color& background = Color::literals::Black) noexcept;
-
         void draw(void) noexcept;
-        void clear(const Color& = Color::literals::Black) noexcept;
-        ShaderLibrary& getShaderLib(void) noexcept { return shaders; }
+        void clear(Color const&) noexcept;
 
-        ~RenderWindow(void) noexcept = default;
-    private:
-        ShaderLibrary shaders;
-        std::vector<std::shared_ptr<Drawable>> drawables;
-        std::chrono::microseconds sleepTime;
-        std::chrono::steady_clock::time_point lastTime;
+        ShaderLibrary                               shaders;
+        Drawables                                   drawables;
+        Duration                                    sleepTime;
+        TimePoint                                   lastTime;
     };
 
     template <std::derived_from<Drawable> T, typename... Args>
-        requires std::is_constructible_v<T, const std::shared_ptr<Vector2ui>&, Args...>
+        requires std::is_constructible_v<T, RenderWindow::ScenePtr const&, Args...>
     void RenderWindow::emplaceDrawable(Args&&... args) noexcept {
-        auto ptr = std::make_shared<T>(WindowInterface::getWindowDimmensions(), std::forward<Args>(args)...);
+        auto ptr = std::make_shared<T>(WindowInterface::getWindowDimmensions(),
+            std::forward<Args>(args)...);
         if constexpr (std::is_base_of_v<Transformable, T>)
             transformables.push_back(std::static_pointer_cast<Transformable>(ptr));
         drawables.push_back(std::move(ptr));
