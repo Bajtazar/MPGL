56200dc6efc11bf298320389d0ddc1d65ba9fc34
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2021-10-09 13:50:08 +0200

    Fixed Chebyshev polynomial implementation

--- src/Engine/Mathematics/ChebyshevPol.hpp
+++ src/Engine/Mathematics/ChebyshevPol.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
-#include <inttypes.h>
+#include <algorithm>
 #include <array>
 
 namespace ge {
 
-    template <uint16_t Precision>
+    template <uint8_t Precision, uint8_t Size>
+        requires (Size >= Precision)
+    constexpr std::array<double, Precision> cutArray(
+            std::array<double, Size> const& array) noexcept;
+
+    template <uint8_t Precision>
         requires (Precision != 0)
     class ChebyshevPol {
     protected:
@@ -14,19 +19,19 @@ namespace ge {
         constexpr ChebyshevPol(CoefficientArray&& coefficients) noexcept
             : coefficients{coefficients} {}
 
-        constexpr double calculate(double point) noexcept;
+        constexpr double calculate(double point) const noexcept;
     private:
         CoefficientArray                        coefficients;
 
         constexpr double higherPrecision(double point) const noexcept;
     };
 
-    template <uint16_t Precision>
+    template <uint8_t Precision>
         requires (Precision != 0)
     constexpr double ChebyshevPol<Precision>::higherPrecision(double point) const noexcept {
         double prelast = 1., last = point, sum = 0.;
-        for (uint16_t i = 2; i != Precision; ++i) {
-            double current = 2 * point * last - prelast;
+        for (uint8_t i = 2; i != Precision; ++i) {
+            double current = 2. * point * last - prelast;
             prelast = last;
             last = current;
             sum += coefficients[i] * current;
@@ -34,17 +39,24 @@ namespace ge {
         return sum;
     }
 
-    template <uint16_t Precision>
+    template <uint8_t Precision>
         requires (Precision != 0)
-    constexpr double ChebyshevPol<Precision>::calculate(double point) noexcept {
-        double sum;
-        if constexpr (Precision == 1)
-            sum = coefficients[0];
-        else if constexpr (Precision == 2)
-            sum = point * coefficients[1] + coefficients[0];
-        if constexpr(Precision > 2)
+    constexpr double ChebyshevPol<Precision>::calculate(double point) const noexcept {
+        double sum = coefficients[0];
+        if constexpr (Precision >= 2)
+            sum += point * coefficients[1];
+        if constexpr (Precision > 2)
             sum += higherPrecision(point);
         return sum;
     }
 
+    template <uint8_t Precision, uint8_t Size>
+        requires (Size >= Precision)
+    constexpr std::array<double, Precision>
+        cutArray(std::array<double, Size> const& array) noexcept
+    {
+        std::array<double, Precision> result;
+        std::copy(array.begin(), array.begin() + Precision, result.begin());
+        return result;
+    }
 }
