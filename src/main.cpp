50241a0d36bfae536bb3ebdfdbdbe774e8a889c8
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2022-02-20 01:23:48 +0100

    Extended the example code

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,42 @@
 
 using namespace mpgl;
 
+class RotatingSprite : public Drawable,
+    public TickEvent, public ScreenTransformationEvent
+{
+public:
+    RotatingSprite(uint32 rotationTime,
+            Texture const& texture)
+        : square{texture, 100_x + 100_y, 100_x + 100_y},
+        rotationTime{rotationTime}
+    {
+        square[0].color = Color::Red;
+        square[1].color = Color::Green;
+        square[2].color = Color::Blue;
+        square[3].color = Color::Yellow;
+    }
+
+    void onTick(std::chrono::milliseconds const& duration) noexcept {
+        square.rotate(150_x + 150_y,
+            2 * std::numbers::pi / (1000 * rotationTime)
+            * duration.count());
+    }
+
+    void onScreenTransformation(Vector2u const& oldDim) noexcept
+        { square.onScreenTransformation(oldDim); }
+
+    void copyToGPU(void) const noexcept
+        { square.copyToGPU(); }
+
+    void draw(void) const noexcept
+        { square.draw(); }
+
+    ~RotatingSprite(void) noexcept = default;
+private:
+    Sprite<true>                            square;
+    uint32 const                            rotationTime;
+};
+
 int main(void) noexcept {
     Window window{640_x + 480_y, "Hello World!"};
     window.setFPSLimit(140);
@@ -55,10 +91,7 @@ int main(void) noexcept {
     array.emplace_back(Color::White, 400_x + 400_y, 300_x + 300_y, 400_x + 300_y);
     array.emplace_back(Color::Green, 200_x + 200_y, 200_x + 300_y, 300_x + 200_y);
 
-    window.emplaceDrawable<DefaultSprite>(pack["pad.jpeg"], 100_x + 100_y, 100_x + 100_y);
-    auto& sprite = dynamic_cast<DefaultSprite&>(*window[3]);
-    sprite.rotate(150_x + 150_y, std::numbers::pi_v<float64> / 4);
-
+    window.emplaceDrawable<RotatingSprite>(4, pack["pad.jpeg"]);
     window.emplaceDrawable<Ellipse>(200_x + 400_y, 40_x + 20_y, Color::Red);
 
     return window.windowLoop(Color{0.2f, 0.3f, 0.3f, 1.f});
