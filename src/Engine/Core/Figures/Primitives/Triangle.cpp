4a69c4728b9c143e00a3674875d3fb3c4c4e4ceb
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2022-03-06 20:40:34 +0100

    Improved Triangle implementation

--- src/Engine/Core/Figures/Primitives/Triangle.cpp
+++ src/Engine/Core/Figures/Primitives/Triangle.cpp
@@ -1,5 +1,31 @@
+/**
+ *  MPGL - Modern and Precise Graphics Library
+ *
+ *  Copyright (c) 2021-2022
+ *      Grzegorz Czarnecki (grzegorz.czarnecki.2021@gmail.com)
+ *
+ *  This software is provided 'as-is', without any express or
+ *  implied warranty. In no event will the authors be held liable
+ *  for any damages arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any
+ *  purpose, including commercial applications, and to alter it and
+ *  redistribute it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented;
+ *  you must not claim that you wrote the original software.
+ *  If you use this software in a product, an acknowledgment in the
+ *  product documentation would be appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such,
+ *  and must not be misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source
+ *  distribution
+ */
 #include "Triangle.hpp"
-#include "../Views.hpp"
+
+#include "../../Context/Buffers/BindGuard.hpp"
 
 namespace mpgl {
 
@@ -14,27 +40,11 @@ namespace mpgl {
     Triangle::Triangle(Color const& color)
         : Angular{3, color} {}
 
-    Triangle::Triangle(Triangle const& triangle)
-        : Angular{triangle} {}
-
-    Triangle& Triangle::operator= (Triangle const& triangle) {
-        Angular::operator=(triangle);
-        return *this;
-    }
-
-    Triangle::Triangle(Triangle&& triangle) noexcept
-        : Angular{std::move(triangle)} {}
-
-    Triangle& Triangle::operator= (Triangle&& triangle) noexcept {
-        Angular::operator=(std::move(triangle));
-        return *this;
-    }
-
     void Triangle::draw(void) const noexcept {
+        actualizeBufferBeforeDraw();
         shaderProgram->use();
-        glBindVertexArray(vertexArrayObject);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glBindVertexArray(0);
+        BindGuard<VertexArray> vaoGuard{vertexArray};
+        vertexArray.drawArrays(VertexArray::DrawMode::Triangles, 3);
     }
 
 }
