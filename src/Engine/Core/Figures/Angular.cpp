92d2f55540cbe9dea1ef7f095661ab302b9e5c43
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2022-03-06 20:25:30 +0100

    Modified Angular implementation

--- src/Engine/Core/Figures/Angular.cpp
+++ src/Engine/Core/Figures/Angular.cpp
@@ -1,92 +1,140 @@
+/**
+ *  MPGL - Modern and Precise Graphics Library
+ *
+ *  Copyright (c) 2021-2022
+ *      Grzegorz Czarnecki (grzegorz.czarnecki.2021@gmail.com)
+ *
+ *  This software is provided 'as-is', without any express or
+ *  implied warranty. In no event will the authors be held liable
+ *  for any damages arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any
+ *  purpose, including commercial applications, and to alter it and
+ *  redistribute it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented;
+ *  you must not claim that you wrote the original software.
+ *  If you use this software in a product, an acknowledgment in the
+ *  product documentation would be appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such,
+ *  and must not be misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source
+ *  distribution
+ */
+#include "Views.hpp"
 #include "Angular.hpp"
 #include "../../Mathematics/Systems.hpp"
+#include "../Context/Buffers/BindGuard.hpp"
 
 namespace mpgl {
 
+    void Angular::attachBuffers(void) const noexcept {
+        vertexArray.bind();
+        vertexBuffer.bind();
+    }
+
+    void Angular::setBuffers(void) const noexcept {
+        vertexBuffer.setBufferData(vertices);
+        vertexArray.setArrayData(vertices.front());
+    }
+
+    void Angular::detachBuffers(void) const noexcept {
+        vertexBuffer.unbind();
+        vertexArray.unbind();
+    }
+
+    void Angular::initializeBuffers(void) const noexcept {
+        attachBuffers();
+        setBuffers();
+        detachBuffers();
+    }
+
     Angular::Angular(size_t size, Color const& color)
-        : Figure{"2DDefault"}, vertices{size, Vertex{{}, color}} {}
+        : Figure{"2DDefault"}, vertices{size,
+            Vertex{Vector2f{}, color}}
+    {
+        initializeBuffers();
+    }
 
     Angular::Angular(Vertices vertices) : Figure{"2DDefault"},
-        vertices{std::move(vertices)} {}
-
-    Angular::Angular(Angular&& shape) noexcept
-        : Figure{std::move(shape)}, vertices{
-            std::move(shape.vertices)} {}
+        vertices{std::move(vertices)}
+    {
+        initializeBuffers();
+    }
 
     Angular::Angular(Angular const& shape)
-        : Figure{shape}, vertices{shape.vertices} {}
+        : Figure{shape}, vertices{shape.vertices}
+    {
+        initializeBuffers();
+    }
 
-    Angular& Angular::operator=(Angular&& shape) noexcept {
-        Figure::operator=(std::move(shape));
-        vertices = std::move(shape.vertices);
-        return *this;
+    void Angular::actualizeBufferBeforeDraw(void) const noexcept {
+        if (modified) {
+            {
+                BindGuard<VertexBuffer> vboGuard{vertexBuffer};
+                vertexBuffer.changeBufferData(vertices);
+            }
+            modified = false;
+        }
     }
 
     Angular& Angular::operator=(Angular const& shape) {
         Figure::operator=(shape);
-        vertices = shape.vertices;
+        vertices.clear();
+        vertices.reserve(shape.vertices.size());
+        std::ranges::copy(shape.vertices, std::back_inserter(vertices));
+        modified = true;
         return *this;
     }
 
-    void Angular::copyToGPU(void) const noexcept {
-        bindBuffers();
-        copyBuffersToGPU();
-        unbindBuffers();
-    }
-
-    void Angular::bindBuffers(void) const noexcept {
-        glBindVertexArray(vertexArrayObject);
-        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-        glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float32) * vertices.size(),
-            vertices.data(), GL_STATIC_DRAW);
-    }
-
-    void Angular::copyBuffersToGPU(void) const noexcept {
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), nullptr);
-        glEnableVertexAttribArray(0);
-
-        glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex),
-            reinterpret_cast<void*>(2 * sizeof(float32)));
-        glEnableVertexAttribArray(1);
-    }
-
-    void Angular::unbindBuffers(void) const noexcept {
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glBindVertexArray(0);
-    }
-
-    void Angular::onScreenTransformation(Vector2u const& oldDimensions) noexcept {
-        for (auto& vertexPosition : vertices | std::views::transform(&Angular::Vertex::position)) {
+    void Angular::onScreenTransformation(
+        Vector2u const& oldDimensions) noexcept
+    {
+        for (auto& vertexPosition : vertices | views::position) {
             Vector2f& position = vertexPosition.get();
-            position = (position + 1.f) * vectorCast<float32>(oldDimensions)
-                / vectorCast<float32>(context.windowDimensions) - 1.f;
+            position = (position + 1.f) * vectorCast<float32>(
+                oldDimensions) / vectorCast<float32>(
+                    context.windowDimensions) - 1.f;
         }
-        copyToGPU();
+        modified = true;
     }
 
     void Angular::translate(Vector2f const& shift) noexcept {
-        for (auto& vertexPosition : vertices | std::views::transform(&Angular::Vertex::position))
-            vertexPosition = static_cast<Vector2f>(vertexPosition) + shift;
-        copyToGPU();
+        for (auto& vertexPosition : vertices | views::position)
+            vertexPosition = static_cast<Vector2f>(
+                vertexPosition) + shift;
+        modified = true;
     }
 
-    void Angular::rotate(Vector2f const& center, float32 angle) noexcept {
+    void Angular::rotate(
+        Vector2f const& center,
+        float32 angle) noexcept
+    {
         rotate(center, rotationMatrix<float32>(angle));
     }
 
-    void Angular::rotate(Vector2f const& center, Matrix2f const& rotation) noexcept {
-        for (auto& vertexPosition : vertices | std::views::transform(&Angular::Vertex::position)) {
+    void Angular::rotate(
+        Vector2f const& center,
+        Matrix2f const& rotation) noexcept
+    {
+        for (auto& vertexPosition : vertices | views::position) {
             Vector2f position = vertexPosition;
             Vector2f radius = position - center;
             vertexPosition = rotation * radius + center;
         }
-        copyToGPU();
+        modified = true;
     }
 
-    void Angular::scale(Vector2f const& center, float32 factor) noexcept {
-        for (auto& vertexPosition : vertices | std::views::transform(&Angular::Vertex::position))
-            vertexPosition = (static_cast<Vector2f>(vertexPosition) - center) * factor + center;
-        copyToGPU();
+    void Angular::scale(
+        Vector2f const& center,
+        float32 factor) noexcept
+    {
+        for (auto& vertexPos : vertices | views::position)
+            vertexPos = (static_cast<Vector2f>(vertexPos) - center
+                ) * factor + center;
+        modified = true;
     }
 
 }
--- src/Engine/Core/Figures/Angular.hpp
+++ src/Engine/Core/Figures/Angular.hpp
@@ -1,61 +1,154 @@
+/**
+ *  MPGL - Modern and Precise Graphics Library
+ *
+ *  Copyright (c) 2021-2022
+ *      Grzegorz Czarnecki (grzegorz.czarnecki.2021@gmail.com)
+ *
+ *  This software is provided 'as-is', without any express or
+ *  implied warranty. In no event will the authors be held liable
+ *  for any damages arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any
+ *  purpose, including commercial applications, and to alter it and
+ *  redistribute it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented;
+ *  you must not claim that you wrote the original software.
+ *  If you use this software in a product, an acknowledgment in the
+ *  product documentation would be appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such,
+ *  and must not be misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source
+ *  distribution
+ */
 #pragma once
 
 #include "Figure.hpp"
 #include "../Color.hpp"
 #include "../../Utility/Adapter.hpp"
-#include "../Transformations/Transformable2D.hpp"
+#include "../Context/Buffers/Vertex.hpp"
 
 namespace mpgl {
 
+    /**
+     * Base class for all angular shapes
+     */
     class Angular : public Figure {
     public:
-        struct Vertex {
-            explicit Vertex(Vector2f const& position,
-                Color const& color) noexcept
-                    : position{position}, color{color} {}
-
-            Adapter<Vector2f>               position;
-            Color                           color;
-
-            template <std::size_t Index>
-                requires (Index < 2)
-            constexpr auto&& get(void) & noexcept;
-
-            template <std::size_t Index>
-                requires (Index < 2)
-            constexpr auto&& get(void) const& noexcept;
-        };
+        /// The vertex used by all angular shapes
+        using Vertex = mpgl::Vertex<
+            VertexComponent<"position", Adapter<Vector2f>,
+                DataType::Float32>,
+            VertexComponent<"color", Color, DataType::Float32>
+        >;
 
         typedef std::vector<Vertex>         Vertices;
 
-        void copyToGPU(void) const noexcept final;
+        /**
+         * Pure virtual function. Has to be overloaded.
+         * Allows to draw an object
+         */
         virtual void draw(void) const noexcept = 0;
 
+        /**
+         * Transforms the figure during the screen
+         * transformation event
+         *
+         * @param oldDimensions the old screen dimensions
+         */
         void onScreenTransformation(
             Vector2u const& oldDimensions) noexcept final;
+
+        /**
+         * Translates the figure by the given shift vector
+         *
+         * @param shift the shift vector
+         */
         void translate(Vector2f const& shift) noexcept final;
+
+        /**
+         * Scales the figure around given center by
+         * a given factor
+         *
+         * @param center the scale center
+         * @param factor the scale factor
+         */
         void scale(Vector2f const& center,
             float32 factor) noexcept final;
+
+        /**
+         * Rotates the figure around given point by
+         * the given angle counter clockwise
+         *
+         * @param center the rotation point
+         * @param angle the rotation angle [in rads]
+         */
         void rotate(Vector2f const& center,
             float32 angle) noexcept final;
+
+        /**
+         * Rotates the figure around given point using
+         * given matrix
+         *
+         * @param center the rotation point
+         * @param rotation the rotation matrix
+         */
         void rotate(Vector2f const& center,
             Matrix2f const& rotation) noexcept final;
 
+        /**
+         * Returns the reference to vertex with the given index
+         *
+         * @param index the vertex's index
+         * @return the reference to vertex with the given index
+         */
         Vertex& operator[] (std::size_t index) noexcept
-            { return vertices[index]; }
+            { modified = true; return vertices[index]; }
+
+        /**
+         * Returns the constant reference to vertex with
+         * the given index
+         *
+         * @param index the vertex's index
+         * @return the constant reference to vertex with
+         * the given index
+         */
         Vertex const& operator[] (std::size_t index) const noexcept
             { return vertices[index]; }
 
+        /**
+         * Returns the reference to the front vertex
+         *
+         * @return the reference to the front vertex
+         */
         Vertex& front(void) noexcept
-            { return vertices.front(); }
+            { modified = true; return vertices.front(); }
+
+        /**
+         * Returns the constant reference to the front vertex
+         *
+         * @return the constant reference to the front vertex
+         */
         Vertex const& front(void) const noexcept
             { return vertices.front(); }
 
+        /**
+         * Returns the reference to the back vertex
+         *
+         * @return the reference to the back vertex
+         */
         Vertex& back(void) noexcept
-            { return vertices.back(); }
+            { modified = true; return vertices.back(); }
+
+        /**
+         * Returns the constant reference to the back vertex
+         *
+         * @return the constant reference to the back vertex
+         */
         Vertex const& back(void) const noexcept
             { return vertices.back(); }
-
 
         using iterator                    = Vertices::iterator;
         using const_iterator              = Vertices::const_iterator;
@@ -64,73 +157,198 @@ namespace mpgl {
         using const_reverse_iterator
             = Vertices::const_reverse_iterator;
 
+        /**
+         * Returns the number of vertices in the object
+         *
+         * @return the number of vertices
+         */
         std::size_t size(void) const noexcept
             { return vertices.size(); }
 
+        /**
+         * Returns the iterator to the begining of the vertices
+         *
+         * @return the iterator to the begining of the vertices
+         */
         iterator begin(void) noexcept
-            { return vertices.begin(); }
-        iterator end(void) noexcept
-            { return vertices.end(); }
+            { modified = true; return vertices.begin(); }
 
+        /**
+         * Returns the iterator to the end of the vertices
+         *
+         * @return the iterator to the end of the vertices
+         */
+        iterator end(void) noexcept
+            { modified = true; return vertices.end(); }
+
+        /**
+         * Returns the constant iterator to the begining
+         * of the vertices
+         *
+         * @return the constant iterator to the begining
+         * of the vertices
+         */
         const_iterator begin(void) const noexcept
             { return vertices.begin(); }
+
+        /**
+         * Returns the constant iterator to the end
+         * of the vertices
+         *
+         * @return the constant iterator to the end
+         * of the vertices
+         */
         const_iterator end(void) const noexcept
             { return vertices.end(); }
 
+        /**
+         * Returns the constant iterator to the begining
+         * of the vertices
+         *
+         * @return the constant iterator to the begining
+         * of the vertices
+         */
         const_iterator cbegin(void) const noexcept
             { return vertices.begin(); }
+
+        /**
+         * Returns the constant iterator to the end
+         * of the vertices
+         *
+         * @return the constant iterator to the end
+         * of the vertices
+         */
         const_iterator cend(void) const noexcept
             { return vertices.end(); }
 
+        /**
+         * Returns the reverse iterator to the end of
+         * the vertices
+         *
+         * @return the reverse iterator to the end of
+         * the vertices
+         */
         reverse_iterator rbegin(void) noexcept
-            { return vertices.rbegin(); }
-        reverse_iterator rend(void) noexcept
-            { return vertices.rend(); }
+            { modified = true; return vertices.rbegin(); }
 
+        /**
+         * Returns the reverse iterator to the begining of
+         * the vertices
+         *
+         * @return the reverse iterator to the begining of
+         * the vertices
+         */
+        reverse_iterator rend(void) noexcept
+            { modified = true; return vertices.rend(); }
+
+        /**
+         * Returns the constant reverse iterator to the end of
+         * the vertices
+         *
+         * @return the constant reverse iterator to the end of
+         * the vertices
+         */
         const_reverse_iterator rbegin(void) const noexcept
             { return vertices.rbegin(); }
+
+        /**
+         * Returns the constant reverse iterator to the begining of
+         * the vertices
+         *
+         * @return the constant reverse iterator to the begining of
+         * the vertices
+         */
         const_reverse_iterator rend(void) const noexcept
             { return vertices.rend(); }
 
+        /**
+         * Returns the constant reverse iterator to the end of
+         * the vertices
+         *
+         * @return the constant reverse iterator to the end of
+         * the vertices
+         */
         const_reverse_iterator crbegin(void) const noexcept
             { return vertices.crbegin(); }
+
+        /**
+         * Returns the constant reverse iterator to the begining of
+         * the vertices
+         *
+         * @return the constant reverse iterator to the begining of
+         * the vertices
+         */
         const_reverse_iterator crend(void) const noexcept
             { return vertices.crend(); }
 
+        /**
+         * Virtual destructor. Destroy the Angular object
+         */
         virtual ~Angular(void) noexcept = default;
     protected:
+        /**
+         * Construct a new Angular object. Creates the
+         * vertices vector of a given size with a given color
+         *
+         * @param size the size of the vertices vector
+         * @param color the color of the vertices
+         */
         explicit Angular(size_t size, Color const& color);
+
+        /**
+         * Construct a new Angular object with given
+         * vector of vertices
+         *
+         * @param vertices the vector of vertices
+         */
         explicit Angular(Vertices vertices);
 
+        /**
+         * Construct a new Angular object from a given
+         * constant reference
+         *
+         * @param shape the given angular constant reference
+         */
         explicit Angular(Angular const& shape);
-        explicit Angular(Angular&& shape) noexcept;
 
+        explicit Angular(Angular&& shape) noexcept = default;
+
+        /**
+         * Assigns the given constant reference to the object
+         *
+         * @param shape the given angular constant reference
+         * @return the reference to this object
+         */
         Angular& operator=(Angular const& shape);
-        Angular& operator=(Angular&& shape) noexcept;
 
-        virtual void bindBuffers(void) const noexcept;
-        virtual void unbindBuffers(void) const noexcept;
-        void copyBuffersToGPU(void) const noexcept;
+        Angular& operator=(Angular&& shape) noexcept = default;
+
+        /**
+         * Initializes inner buffers
+         */
+        void initializeBuffers(void) const noexcept;
+
+        /**
+         * Actualizes vertex buffer before draw
+         */
+        void actualizeBufferBeforeDraw(void) const noexcept;
+
+        /**
+         * Virtual method. Attaches buffers before operations
+         */
+        virtual void attachBuffers(void) const noexcept;
+
+        /**
+         * Virtual method. Sets the buffers data
+         */
+        virtual void setBuffers(void) const noexcept;
+
+        /**
+         * Virtual method. Detaches buffers after operations
+         */
+        virtual void detachBuffers(void) const noexcept;
 
         Vertices                            vertices;
     };
-
-    template <std::size_t Index>
-        requires (Index < 2)
-    constexpr auto&& Angular::Vertex::get(void) & noexcept {
-        if constexpr(Index)
-            return color;
-        else
-            return position;
-    }
-
-    template <std::size_t Index>
-        requires (Index < 2)
-    constexpr auto&& Angular::Vertex::get(void) const& noexcept {
-        if constexpr(Index)
-            return color;
-        else
-            return position;
-    }
 
 }
