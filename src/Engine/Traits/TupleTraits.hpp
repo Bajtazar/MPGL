26417cb9f8ef6212d0350a7aad5e0b61e8b6a19f
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2022-03-17 16:21:42 +0100

    Refactored tuple traits

--- src/Engine/Traits/TupleTraits.hpp
+++ src/Engine/Traits/TupleTraits.hpp
@@ -1,3 +1,28 @@
+/**
+ *  MPGL - Modern and Precise Graphics Library
+ *
+ *  Copyright (c) 2021-2022
+ *      Grzegorz Czarnecki (grzegorz.czarnecki.2021@gmail.com)
+ *
+ *  This software is provided 'as-is', without any express or
+ *  implied warranty. In no event will the authors be held liable
+ *  for any damages arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any
+ *  purpose, including commercial applications, and to alter it and
+ *  redistribute it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented;
+ *  you must not claim that you wrote the original software.
+ *  If you use this software in a product, an acknowledgment in the
+ *  product documentation would be appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such,
+ *  and must not be misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source
+ *  distribution
+ */
 #pragma once
 
 #include "Concepts.hpp"
@@ -6,42 +31,107 @@
 
 namespace mpgl {
 
-    class  TupleHelperFn {
+    /**
+     * Helper metastruct. Helps in creation of mutlidimensional
+     * tensor-like tuples
+     */
+    class TupleHelperFn {
     private:
+        /**
+         * Meta method. Allows to get the tuple type containing
+         * the given number of types
+         *
+         * @tparam Tp the tuple base type
+         * @tparam I the index sequence
+         * @return the tuple consisting of the given number of
+         * base types
+         */
         template <PureType Tp, std::size_t... I>
         static constexpr auto tupleConstructor(
             std::index_sequence<I...>) noexcept
                 -> decltype(std::tuple{(I, std::declval<Tp>())...});
     public:
-        template <PureType Tp, std::size_t Dimm>
-            requires (Dimm != 0)
+        /**
+         * Meta method. Allows to get the tuple type containing
+         * the given number of types
+         *
+         * @tparam Tp the tuple base type
+         * @tparam Dim the tuple dimensions
+         * @return the tuple consisting of the given number of
+         * base types
+         */
+        template <PureType Tp, std::size_t Dim>
+            requires (Dim != 0)
         static constexpr auto tensorConstructor(void) noexcept
             -> decltype(tupleConstructor<Tp>(
-                std::make_index_sequence<Dimm>{}));
+                std::make_index_sequence<Dim>{}));
 
-        template <PureType Tp, std::size_t Dimm, std::size_t... Dimensions>
-            requires (Dimm != 0 && sizeof...(Dimensions) > 0)
+        /**
+         * Meta method. Allows to get the tensor-like tuple
+         * constructed from the given number of types in
+         * earch dimension
+         *
+         * @tparam Tp the tuple base type
+         * @tparam Dim the first-dimension tuple dimensions
+         * @tparam the rest dimensions
+         * @return the tensor-like tuple
+         */
+        template <PureType Tp, std::size_t Dim,
+            std::size_t... Dimensions>
+                requires (Dim != 0 && sizeof...(Dimensions) > 0)
         static constexpr auto tensorConstructor(void) noexcept
             -> decltype(tensorConstructor<decltype(
-            tensorConstructor<Tp, Dimensions...>()), Dimm>());
+            tensorConstructor<Tp, Dimensions...>()), Dim>());
     };
 
+    /**
+     * Creates a tuple with reversed arguments
+     *
+     * @tparam Args the arguments types
+     * @param args the arguments
+     * @return the tuple with reversed arguments
+     */
     template <typename... Args>
     constexpr auto tupleReverser(Args&&... args) noexcept {
-        return []<std::size_t... I>(std::tuple<std::remove_reference_t<Args>...> tuple,
-                std::index_sequence<I...>)
+        return []<std::size_t... I>(std::tuple<
+            std::remove_reference_t<Args>...> tuple,
+            std::index_sequence<I...>)
         {
-            return std::tuple{std::get<sizeof...(Args) - I - 1>(tuple)...};
-        }(std::tuple{std::forward<Args>(args)...}, std::make_index_sequence<sizeof...(Args)>{});
+            return std::tuple{std::get<sizeof...(Args) - I - 1>(
+                tuple)...};
+        }(std::tuple{std::forward<Args>(args)...},
+            std::make_index_sequence<sizeof...(Args)>{});
     }
 
-    template <PureType T, std::size_t Size>
-    using UniformTuple = decltype(TupleHelperFn::tensorConstructor<T, Size>());
+    /**
+     * The convenient for shortcut the uniform tuple [one
+     * dimensional tuple]
+     *
+     * @tparam Tp the tuple's base type
+     * @tparam Size the tuple size
+     */
+    template <PureType Tp, std::size_t Size>
+    using UniformTuple = decltype(TupleHelperFn::tensorConstructor<Tp,
+        Size>());
 
-    template <PureType T, std::size_t... Dimensions>
-    using TensorTuple = decltype(TupleHelperFn::tensorConstructor<T, Dimensions...>());
+    /**
+     * The convenient shortcut for the tensor tuple
+     * [mutlidimensional tuple]
+     *
+     * @tparam Tp the tuple's base type
+     * @tparam Dimensions the tuple dimensions
+     */
+    template <PureType Tp, std::size_t... Dimensions>
+    using TensorTuple = decltype(TupleHelperFn::tensorConstructor<Tp,
+        Dimensions...>());
 
+    /**
+     * The convenient shortcut for the reversed tuple
+     *
+     * @tparam Args the tuple arguments [to be reversed]
+     */
     template <typename... Args>
-    using ReversedTuple = decltype(tupleReverser<Args...>(std::declval<Args>()...));
+    using ReversedTuple = decltype(tupleReverser<Args...>(
+        std::declval<Args>()...));
 
 }
