695f7f6621d3265d3d8e59e9bf2cea27e8c4d817
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2022-04-02 17:10:16 +0200

    Refactored the universal register

--- src/Engine/Core/Registers/UniversalRegister.hpp
+++ src/Engine/Core/Registers/UniversalRegister.hpp
@@ -1,3 +1,28 @@
+/**
+ *  MPGL - Modern and Precise Graphics Library
+ *
+ *  Copyright (c) 2021-2022
+ *      Grzegorz Czarnecki (grzegorz.czarnecki.2021@gmail.com)
+ *
+ *  This software is provided 'as-is', without any express or
+ *  implied warranty. In no event will the authors be held liable
+ *  for any damages arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any
+ *  purpose, including commercial applications, and to alter it and
+ *  redistribute it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented;
+ *  you must not claim that you wrote the original software.
+ *  If you use this software in a product, an acknowledgment in the
+ *  product documentation would be appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such,
+ *  and must not be misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source
+ *  distribution
+ */
 #pragma once
 
 #include <vector>
@@ -11,81 +36,45 @@
 
 namespace mpgl {
 
-    template <Event T, typename Signature, Signature Method>
-    class UniversalRegister : public RegisterInterface {
-        UniversalRegister(void) noexcept = delete;
-    };
+    template <Event Tp, typename Signature, Signature Method>
+    class UniversalRegister;
 
-    template <Event T, typename... Args, void(T::*EventMethod)(Args...)>
-    class UniversalRegister<T, void(T::*)(Args...), EventMethod>
-        : public RegisterInterface
+    /**
+     * Register that allows to construct the simple register for
+     * cutom event
+     */
+    template <Event Tp, typename... Args,
+        void(Tp::*EventMethod)(Args...)>
+    struct UniversalRegister<Tp, void(Tp::*)(Args...), EventMethod>
+        : public RegisterInterface<Tp>
     {
-    public:
-        typedef std::shared_ptr<T>          pointer;
-        typedef std::vector<pointer>        Storage;
-        typedef T                           value_type;
-
+        /**
+         * Constructs a new Universal Register object
+         */
         explicit UniversalRegister(void) noexcept = default;
 
-        void pushBack(pointer const& pointer)
-            { storage.push_back(pointer); }
-        void pushBack(pointer&& pointer) noexcept
-            { storage.push_back(std::move(pointer)); }
-
+        /**
+         * Calls the event on the underlying objects with
+         * the given arguments
+         *
+         * @param args the event arguments
+         */
         void onEvent(Args&&... args) noexcept;
 
-        using iterator =                    typename Storage::iterator;
-        using const_iterator =              typename Storage::const_iterator;
-        using reverse_iterator =            typename Storage::reverse_iterator;
-        using const_reverse_iterator =      typename Storage::const_reverse_iterator;
-
-        iterator begin(void) noexcept
-            { return storage.begin(); }
-        iterator end(void) noexcept
-            { return storage.end(); }
-        const_iterator begin(void) const noexcept
-            { return storage.begin(); }
-        const_iterator end(void) const noexcept
-            { return storage.end(); }
-        const_iterator cbegin(void) const noexcept
-            { return storage.cbegin(); }
-        const_iterator cend(void) const noexcept
-            { return storage.cend(); }
-
-        reverse_iterator rbegin(void) noexcept
-            { return storage.rbegin(); }
-        reverse_iterator rend(void) noexcept
-            { return storage.rend(); }
-        const_reverse_iterator rbegin(void) const noexcept
-            { return storage.rbegin(); }
-        const_reverse_iterator rend(void) const noexcept
-            { return storage.rend(); }
-        const_reverse_iterator crbegin(void) const noexcept
-            { return storage.crbegin(); }
-        const_reverse_iterator crend(void) const noexcept
-            { return storage.crend(); }
-
-        std::size_t size(void) const noexcept
-            { return storage.size(); }
-        bool empty(void) const noexcept
-            { return storage.empty(); }
-
-        pointer& operator[] (std::size_t index) noexcept
-            { return storage[index]; }
-        pointer const& operator[] (std::size_t index) const noexcept
-            { return storage[index]; }
-
+        /**
+         * Destroys the Universal Register object
+         */
         ~UniversalRegister(void) noexcept = default;
-    private:
-        Storage                             storage;
     };
 
-    template <Event T, typename... Args, void(T::*EventMethod)(Args...)>
-    void UniversalRegister<T, void(T::*)(Args...), EventMethod>::onEvent(
-        Args&&... args) noexcept
+    template <Event Tp, typename... Args,
+        void(Tp::*EventMethod)(Args...)>
+    void UniversalRegister<Tp, void(Tp::*)(Args...),
+        EventMethod>::onEvent(Args&&... args) noexcept
     {
-        std::ranges::for_each(storage, [...args = std::forward<Args>(args)]
-            (auto& event){ (event.get()->*EventMethod)(args...); });
+        std::ranges::for_each(storage, [...args
+            = std::forward<Args>(args)]
+                (auto& event){ (event.get()->*EventMethod)(args...); });
     }
 
 }
