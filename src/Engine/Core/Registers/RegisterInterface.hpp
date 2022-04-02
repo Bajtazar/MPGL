8c6a7344416d34722c66322c0a6b1abcfda5cb68
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2022-04-02 16:44:20 +0200

    Added conments to register interface

--- src/Engine/Core/Registers/RegisterInterface.hpp
+++ src/Engine/Core/Registers/RegisterInterface.hpp
@@ -1,11 +1,54 @@
+/**
+ *  MPGL - Modern and Precise Graphics Library
+ *
+ *  Copyright (c) 2021-2022
+ *      Grzegorz Czarnecki (grzegorz.czarnecki.2021@gmail.com)
+ *
+ *  This software is provided 'as-is', without any express or
+ *  implied warranty. In no event will the authors be held liable
+ *  for any damages arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any
+ *  purpose, including commercial applications, and to alter it and
+ *  redistribute it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented;
+ *  you must not claim that you wrote the original software.
+ *  If you use this software in a product, an acknowledgment in the
+ *  product documentation would be appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such,
+ *  and must not be misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source
+ *  distribution
+ */
 #pragma once
 
 namespace mpgl {
 
-    struct RegisterInterface {
-        explicit RegisterInterface(void) noexcept = default;
+    /**
+     * Base class for all of the registers
+     */
+    class RegisterInterface {
+    public:
+        RegisterInterface(RegisterInterface const&) = delete;
+        RegisterInterface(RegisterInterface&&) = delete;
 
+        RegisterInterface& operator=(
+            RegisterInterface const&) = delete;
+        RegisterInterface& operator=(
+            RegisterInterface&&) = delete;
+
+        /**
+         * Destroys the Register Interface object
+         */
         virtual ~RegisterInterface(void) noexcept = default;
+    protected:
+        /**
+         * Constructs a new Register Interface object
+         */
+        explicit RegisterInterface(void) noexcept = default;
     };
 
 }
