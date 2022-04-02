ee1762db808277debb588f552864fefe3a40c995
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2022-04-02 20:39:00 +0200

    Refactored bit reversion

--- src/Engine/Utility/BitReversion.hpp
+++ src/Engine/Utility/BitReversion.hpp
@@ -1,3 +1,28 @@
+/**
+ *  MPGL - Modern and Precise Graphics Library
+ *
+ *  Copyright (c) 2021-2022
+ *      Grzegorz Czarnecki (grzegorz.czarnecki.2021@gmail.com)
+ *
+ *  This software is provided 'as-is', without any express or
+ *  implied warranty. In no event will the authors be held liable
+ *  for any damages arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any
+ *  purpose, including commercial applications, and to alter it and
+ *  redistribute it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented;
+ *  you must not claim that you wrote the original software.
+ *  If you use this software in a product, an acknowledgment in the
+ *  product documentation would be appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such,
+ *  and must not be misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source
+ *  distribution
+ */
 #pragma once
 
 #include <inttypes.h>
@@ -7,10 +32,21 @@
 
 namespace mpgl {
 
+    /**
+     * Reverses the order of the bits in the unsigned integer
+     */
     class BitReversion {
     public:
-        constexpr uint8 operator() (uint8 bits) const noexcept
-            { return lookupTable[bits]; }
+
+        /**
+         * Reverses the order of the bits in the unsigned integer
+         *
+         * @tparam Tp the type of the integer
+         * @param bits the number to revert
+         * @return the reverted number
+         */
+        template <std::unsigned_integral Tp>
+        [[nodiscard]] constexpr Tp operator() (Tp bits) const noexcept;
     private:
         constexpr static const std::array<uint8, 256>   lookupTable = {
             0x00, 0x80, 0x40, 0xc0, 0x20, 0xa0, 0x60, 0xe0,
@@ -49,5 +85,16 @@ namespace mpgl {
     };
 
     inline constexpr BitReversion                       reverseBits;
+
+    template <std::unsigned_integral Tp>
+    [[nodiscard]] constexpr Tp BitReversion::operator() (
+        Tp bits) const noexcept
+    {
+        Tp result{0};
+        for (uint8 i = 0; i < sizeof(Tp); ++i)
+            result |= (Tp) lookupTable[(bits >> (i * 8)) & 0xFF]
+                << (8 * (sizeof(Tp) - i - 1));
+        return result;
+    }
 
 }
