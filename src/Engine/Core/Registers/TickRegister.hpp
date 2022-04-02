529a4eb8a2321a1e66191497355a5b04b7f5e122
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2022-04-02 17:05:09 +0200

    Refactored the tick register

--- src/Engine/Core/Registers/TickRegister.hpp
+++ src/Engine/Core/Registers/TickRegister.hpp
@@ -1,3 +1,28 @@
+/**
+ *  MPGL - Modern and Precise Graphics Library
+ *
+ *  Copyright (c) 2021-2022
+ *      Grzegorz Czarnecki (grzegorz.czarnecki.2021@gmail.com)
+ *
+ *  This software is provided 'as-is', without any express or
+ *  implied warranty. In no event will the authors be held liable
+ *  for any damages arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any
+ *  purpose, including commercial applications, and to alter it and
+ *  redistribute it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented;
+ *  you must not claim that you wrote the original software.
+ *  If you use this software in a product, an acknowledgment in the
+ *  product documentation would be appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such,
+ *  and must not be misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source
+ *  distribution
+ */
 #pragma once
 
 #include "../../Events/TickEvent.hpp"
@@ -9,73 +34,51 @@
 
 namespace mpgl {
 
-    class TickRegister : public RegisterInterface {
+    /**
+     * Register that holds the tick events
+     */
+    class TickRegister : public RegisterInterface<TickEvent> {
     public:
         typedef std::chrono::duration<float64, std::milli>  Duration;
-        typedef std::shared_ptr<TickEvent>                  pointer;
-        typedef TickEvent                                   value_type;
-        typedef std::vector<pointer>                        Storage;
 
+        /**
+         * Constructs a new Tick Register object from the given
+         * period between ticks
+         *
+         * @param period the period between ticks
+         */
         explicit TickRegister(Duration const& period = {}) noexcept;
 
+        /**
+         * Sets a new tick period
+         *
+         * @param period the new tick period
+         */
         void setPeriod(Duration const& period) noexcept
             { this->period = period; }
 
-        void pushBack(pointer const& pointer)
-            { storage.push_back(pointer); }
-        void pushBack(pointer&& pointer) noexcept
-            { storage.push_back(std::move(pointer)); }
+        /**
+         * Returns the tick period
+         *
+         * @return the tick period
+         */
+        [[nodiscard]] Duration const& getPeriod(void) const noexcept
+            { return period; }
 
+        /**
+         * Calls the tick event on the underlying objects
+         */
         void onEvent(void) noexcept;
 
-        using iterator =                    typename Storage::iterator;
-        using const_iterator =              typename Storage::const_iterator;
-        using reverse_iterator =            typename Storage::reverse_iterator;
-        using const_reverse_iterator =      typename Storage::const_reverse_iterator;
-
-        iterator begin(void) noexcept
-            { return storage.begin(); }
-        iterator end(void) noexcept
-            { return storage.end(); }
-        const_iterator begin(void) const noexcept
-            { return storage.begin(); }
-        const_iterator end(void) const noexcept
-            { return storage.end(); }
-        const_iterator cbegin(void) const noexcept
-            { return storage.cbegin(); }
-        const_iterator cend(void) const noexcept
-            { return storage.cend(); }
-
-        reverse_iterator rbegin(void) noexcept
-            { return storage.rbegin(); }
-        reverse_iterator rend(void) noexcept
-            { return storage.rend(); }
-        const_reverse_iterator rbegin(void) const noexcept
-            { return storage.rbegin(); }
-        const_reverse_iterator rend(void) const noexcept
-            { return storage.rend(); }
-        const_reverse_iterator crbegin(void) const noexcept
-            { return storage.crbegin(); }
-        const_reverse_iterator crend(void) const noexcept
-            { return storage.crend(); }
-
-        std::size_t size(void) const noexcept
-            { return storage.size(); }
-        bool empty(void) const noexcept
-            { return storage.empty(); }
-
-        pointer& operator[] (std::size_t index) noexcept
-            { return storage[index]; }
-        pointer const& operator[] (std::size_t index) const noexcept
-            { return storage[index]; }
-
+        /**
+         * Destroys the Tick Register object
+         */
         ~TickRegister(void) noexcept = default;
     private:
         typedef std::chrono::steady_clock                   Clock;
         typedef std::chrono::time_point<Clock>              TimePoint;
 
         Clock                                               clock;
-        Storage                                             storage;
         TimePoint                                           lastTime;
         Duration                                            period;
     };
