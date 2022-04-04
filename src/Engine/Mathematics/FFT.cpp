fc845779120c4af43589c0f71285244fbbc98052
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2022-04-04 21:50:22 +0200

    Added comments to FFT

--- src/Engine/Mathematics/FFT.cpp
+++ src/Engine/Mathematics/FFT.cpp
@@ -1,3 +1,28 @@
+/**
+ *  MPGL - Modern and Precise Graphics Library
+ *
+ *  Copyright (c) 2021-2022
+ *      Grzegorz Czarnecki (grzegorz.czarnecki.2021@gmail.com)
+ *
+ *  This software is provided 'as-is', without any express or
+ *  implied warranty. In no event will the authors be held liable
+ *  for any damages arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any
+ *  purpose, including commercial applications, and to alter it and
+ *  redistribute it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented;
+ *  you must not claim that you wrote the original software.
+ *  If you use this software in a product, an acknowledgment in the
+ *  product documentation would be appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such,
+ *  and must not be misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source
+ *  distribution
+ */
 #include "FFT.hpp"
 
 #ifdef _MSC_VER
@@ -7,14 +32,14 @@
 namespace mpgl {
 
     #ifdef __GNUC__
-    uint16 FFT::convolutionSize(uint8 number) noexcept {
+    FFT::size_type FFT::convolutionSize(size_type number) noexcept {
         uint size = 2 * number + 1;
         // __builtin_clz cannot overflow - it is well defined
         return !(size & (size - 1)) ? size :
             1 << (sizeof(long) * CHAR_BIT - __builtin_clz(size));
     }
     #elif defined(_MSC_VER)
-    uint16 FFT::convolutionSize(uint8 number) noexcept {
+    FFT::size_type FFT::convolutionSize(size_type number) noexcept {
         uint size = 2 * number + 1;
         if (!(size & (size - 1)))
             return size;
@@ -24,26 +49,31 @@ namespace mpgl {
     }
     #endif
 
-    FFT::ComplexVector FFT::generateLookupTable(uint16 size) noexcept {
+    FFT::ComplexVector
+        FFT::generateLookupTable(size_type size) noexcept
+    {
         ComplexVector lookupTable;
         lookupTable.reserve(size);
         lookupTable.emplace_back(1.);
-        for (uint16 i = 1; i < size; ++i)
-            lookupTable.push_back(std::polar(1., (2 * i - 1) * std::numbers::pi / size)
-                * lookupTable.back());
+        for (size_type i = 1; i < size; ++i)
+            lookupTable.push_back(std::polar(1.,
+                (2 * i - 1) * std::numbers::pi / size)
+                    * lookupTable.back());
         return lookupTable;
     }
 
-    FFT::ComplexVector FFT::generateRightSequence(ComplexVector const& lookupTable,
-        uint16 size) noexcept
+    FFT::ComplexVector FFT::generateRightSequence(
+        ComplexVector const& lookupTable,
+        size_type size) noexcept
     {
         ComplexVector rightSequence;
         rightSequence.reserve(size);
-        std::ranges::copy(lookupTable, std::back_inserter(rightSequence));
+        std::ranges::copy(lookupTable,
+            std::back_inserter(rightSequence));
         rightSequence.resize(size - lookupTable.size() + 1);
         rightSequence.reserve(size);
-        std::ranges::copy(std::views::reverse(lookupTable | std::views::drop(1)),
-            std::back_inserter(rightSequence));
+        std::ranges::copy(std::views::reverse(lookupTable
+            | std::views::drop(1)), std::back_inserter(rightSequence));
         return rightSequence;
     }
 
@@ -55,8 +85,9 @@ namespace mpgl {
         std::ranges::transform(leftSequence, rightSequence,
             leftSequence.begin(), std::multiplies{});
         cooleyTukey(leftSequence, 1.); // inversed tranform
-        std::ranges::for_each(leftSequence, [&leftSequence](auto& element)
-            { element /= leftSequence.size(); });
+        std::ranges::for_each(leftSequence,
+            [&leftSequence](auto& element)
+                { element /= leftSequence.size(); });
         return leftSequence;
     }
 
--- src/Engine/Mathematics/FastFunctions.cpp
+++ src/Engine/Mathematics/FastFunctions.cpp
@@ -32,22 +32,22 @@
 namespace mpgl {
 
     #ifdef __GNUC__
-    uint8 fastLog2(uint32 number) noexcept {
+    [[nodiscard]] uint8 fastLog2(uint32 number) noexcept {
         return !number ? 0 : __builtin_ctz(number);
     }
     #elif defined(_MSC_VER)
-    uint8 fastLog2(uint32 number) noexcept {
+    [[nodiscard]] uint8 fastLog2(uint32 number) noexcept {
         DWORD trailing = 0;
         return _BitScanForward(&trailing, number) ? trailing : 0;
     }
     #endif
 
     #ifdef __GNUC__
-    bool isPowerOf2(uint32 number) noexcept {
+    [[nodiscard]] bool isPowerOf2(uint32 number) noexcept {
         return number ? true : __builtin_popcount(number) == 1;
     }
     #elif defined(_MSC_VER)
-    bool isPowerOf2(uint32 number) noexcept {
+    [[nodiscard]] bool isPowerOf2(uint32 number) noexcept {
         return number ? true : __popcnt(number) == 1;
     }
     #endif
