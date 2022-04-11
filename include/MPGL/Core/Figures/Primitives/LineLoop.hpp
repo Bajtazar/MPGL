6ecbf4e53623b043f137023ebd54b44776b404ef
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2022-04-11 23:29:20 +0200

    Splitted LineLoop header between hpp and tpp file

--- include/MPGL/Core/Figures/Primitives/LineLoop.hpp
+++ include/MPGL/Core/Figures/Primitives/LineLoop.hpp
@@ -115,14 +115,6 @@ namespace mpgl {
             std::size_t index) const noexcept;
     };
 
-    template <class ColorTp, AllConvertible<Vector2f>... Vectors>
-        requires std::constructible_from<Color, ColorTp>
-    LineLoop::LineLoop(ColorTp&& color, Vectors&&... vertices)
-        : ResizableAngular{std::forward<Color>(color),
-            std::forward<Vectors>(vertices)...} {}
-
-    template <AllConvertible<Vector2f>... Vectors>
-    LineLoop::LineLoop(Vectors&&... vertices)
-        : ResizableAngular{std::forward<Vectors>(vertices)...} {}
-
 }
+
+#include <MPGL/Core/Figures/Primitives/LineLoop.tpp>
