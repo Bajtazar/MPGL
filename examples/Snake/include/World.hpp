f9b9669e6b92fecc743270cc0f28d6e16cb9e390
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2022-04-12 19:03:18 +0200

    Added World to the example Snake

---
+++ examples/Snake/include/World.hpp
@@ -0,0 +1,66 @@
+/**
+ *  MPGL - Modern and Precise Graphics Library
+ *
+ *  Copyright (c) 2021-2022
+ *      Grzegorz Czarnecki (grzegorz.czarnecki.2021@gmail.com)
+ *
+ *  This software is provided 'as-is', without any express or
+ *  implied warranty. In no event will the authors be held liable
+ *  for any damages arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any
+ *  purpose, including commercial applications, and to alter it and
+ *  redistribute it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented;
+ *  you must not claim that you wrote the original software.
+ *  If you use this software in a product, an acknowledgment in the
+ *  product documentation would be appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such,
+ *  and must not be misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source
+ *  distribution
+ */
+#pragma once
+
+#include <MPGL/Events/ScreenTransformationEvent.hpp>
+#include <MPGL/Core/Figures/Primitives/Tetragon.hpp>
+#include <MPGL/Core/Figures/Primitives/Line.hpp>
+#include <MPGL/Core/DrawableCollection.hpp>
+#include <MPGL/Core/Drawable.hpp>
+
+#include <utility>
+
+class World :
+    public mpgl::ScreenTransformationEvent,
+    public mpgl::Drawable2D
+{
+public:
+    explicit World(void);
+
+    virtual void onScreenTransformation(
+        mpgl::Vector2u const& oldDimensions) const noexcept;
+
+    virtual void draw(void) const noexcept;
+
+    void createNewApple(void) noexcept;
+
+    mpgl::Vector2f const& applePosition(void) const noexcept
+        { return apple.first; }
+
+    ~World(void) noexcept = default;
+private:
+    typedef mpgl::DrawableCollection<
+        mpgl::Line>                             Lines;
+    typedef std::pair<
+        mpgl::Vector2f, mpgl::Tetragon>         AppleTuple;
+
+    Lines                                       table;
+    AppleTuple                                  apple;
+
+    void generateTable(void);
+
+    static mpgl::Vector2f genApplePosition(void) noexcept;
+};
---
+++ examples/Snake/src/World.cpp
@@ -0,0 +1,81 @@
+/**
+ *  MPGL - Modern and Precise Graphics Library
+ *
+ *  Copyright (c) 2021-2022
+ *      Grzegorz Czarnecki (grzegorz.czarnecki.2021@gmail.com)
+ *
+ *  This software is provided 'as-is', without any express or
+ *  implied warranty. In no event will the authors be held liable
+ *  for any damages arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any
+ *  purpose, including commercial applications, and to alter it and
+ *  redistribute it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented;
+ *  you must not claim that you wrote the original software.
+ *  If you use this software in a product, an acknowledgment in the
+ *  product documentation would be appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such,
+ *  and must not be misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source
+ *  distribution
+ */
+#include <World.hpp>
+
+#include <MPGL/Core/Context/Buffers/VertexCast.hpp>
+
+#include <chrono>
+
+using namespace mpgl;
+
+World::World(void)
+    : apple{Vector2f{}, Tetragon{Color::Red}}
+{
+    generateTable();
+    createNewApple();
+}
+
+void World::createNewApple(void) noexcept {
+    apple.first = genApplePosition();
+    auto const& [k, j] = apple.first;
+    apple.second[0] & cast::position
+        = Vector2f{117 + 36*j, 36*(k+3)+9};
+    apple.second[1] & cast::position
+        = Vector2f{99 + 36*(j+1), 36*(k+3)+9};
+    apple.second[2] & cast::position
+        = Vector2f{99 + 36*(j+1), 36*(k+4)-9};
+    apple.second[3] & cast::position
+        = Vector2f{117 + 36*j, 36*(k+4)-9};
+}
+
+Vector2f World::genApplePosition(void) noexcept {
+    uint64_t time = std::chrono::duration_cast<
+        std::chrono::nanoseconds>(std::chrono::system_clock::now(
+            ).time_since_epoch()).count();
+    return {time % 14, time % 24};
+}
+
+void World::generateTable(void) {
+    table.reserve(40);
+    for (uint16_t i = 0;i < 15;i++)
+        table.emplace_back(Vector2f{108, 36*(i+3)},
+            Vector2f{972, 36*(i+3)}, Color::White);
+    for (uint16_t i = 0;i < 25;i++)
+        table.emplace_back(Vector2f{108 + 36*i, 108},
+            Vector2f{108 + 36*i, 612}, Color::White);
+}
+
+void World::onScreenTransformation(
+    Vector2u const& oldDimensions) const noexcept
+{
+    table.onScreenTransformation(oldDimensions);
+    apple.second.onScreenTransformation(oldDimensions);
+}
+
+void World::draw(void) const noexcept {
+    table.draw();
+    apple.second.draw();
+}
