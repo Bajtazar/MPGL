426fe4544ff6a8341d0eb7bb9505d17f4fb97c1d
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2022-06-25 14:49:16 +0200

    Added Delegation Wrapper

---
+++ include/MPGL/Utility/DelegationWrapper.hpp
@@ -0,0 +1,182 @@
+/**
+ *  MPGL - Modern and Precise Graphics Library
+ *
+ *  Copyright (c) 2021-2022
+ *      Grzegorz Czarnecki (grzegorz.czarnecki.2021@gmail.com)
+ *
+ *  This software is provided 'as-is', without any express or
+ *  implied warranty. In no event will the authors be held liable
+ *  for any damages arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any
+ *  purpose, including commercial applications, and to alter it and
+ *  redistribute it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented;
+ *  you must not claim that you wrote the original software.
+ *  If you use this software in a product, an acknowledgment in the
+ *  product documentation would be appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such,
+ *  and must not be misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source
+ *  distribution
+ */
+#include <MPGL/Traits/Concepts.hpp>
+
+#include <memory>
+#include <functional>
+
+namespace mpgl {
+
+    /**
+     * Wrapps the invocable and it's arguments that are supposed to
+     * be executed derefferedly. Stores the resource handles as the
+     * weak pointers. If the resource is not available via the
+     * weak pointer then abandons the execution of the invocable
+     *
+     * @tparam Handles the shared pointer resources types
+     */
+    template <InstanceOf<std::shared_ptr>... Handles>
+    class DelegationWrapper {
+    public:
+        /**
+         * Wrapps the non-handling resources of the delegation wrapper
+         *
+         * @tparam Args the non-handling types
+         */
+        template <PureType... Args>
+        class ArgumentsWrapper {
+        public:
+            /**
+             * Wrapps the invocable. Decides whether to execute
+             * the invocable or not
+             *
+             * @tparam Function the wrapped invocable type
+             */
+            template <std::invocable<Handles..., Args...> Function>
+            class DelegatedFunctor {
+            public:
+                /**
+                 * Constructs a new Delegated Functor object
+                 *
+                 * @param handles the rvalue reference to the
+                 * delegation wrapper
+                 * @param arguments the rvalue reference to the
+                 * arguments wrapper
+                 * @param invocable the rvalue reference to the
+                 * invocable object
+                 */
+                explicit DelegatedFunctor(
+                    DelegationWrapper&& handles,
+                    ArgumentsWrapper&& arguments,
+                    Function&& invocable);
+
+                /**
+                 * Checks whether the resource-handling references
+                 * are still valid. If so then the wrapped invocable
+                 * is executed. If not then nothing is done
+                 */
+                void operator() (void) const;
+            private:
+                /**
+                 * Checks if the resource handlers has expired
+                 *
+                 * @return whether the resource handlers has expired
+                 */
+                bool hasExpired(void) const;
+
+                /**
+                 * Converts the weak resource pointers to the shared
+                 * pointers
+                 *
+                 * @return the tuple with the shared resource-handling
+                 * pointers
+                 */
+                auto replenishSharedPointers(void) const;
+
+                DelegationWrapper       handles;
+                ArgumentsWrapper        arguments;
+                Function                invocable;
+            };
+
+            /**
+             * Constructs a new Arguments Wrapper object
+             *
+             * @param wrapper the rvalue referece to the delegation
+             * wrapper
+             * @param args the non-handling arguments
+             */
+            explicit ArgumentsWrapper(
+                DelegationWrapper&& wrapper,
+                Args... args);
+
+            /**
+             * Creates the functor that handles and manages the given
+             * arguments, resources and invocable. This method cannot
+             * be called more than once
+             *
+             * @tparam Function the invocable type
+             * @param func the rvalue reference to the invocable
+             * @return the delegated functor
+             */
+            template <std::invocable<Handles..., Args...> Function>
+            [[nodiscard]] DelegatedFunctor<Function> operator() (
+                Function&& func);
+        private:
+            typedef std::tuple<Args...> ArgTuple;
+
+            DelegationWrapper           wrapper;
+            ArgTuple                    args;
+        };
+
+        /**
+         * Constructs a new Delegation Wrapper object
+         *
+         * @param handles the resource handling shared pointer types
+         */
+        explicit DelegationWrapper(Handles... handles);
+
+        /**
+         * Creates the functor that handles and manages the given
+         * arguments, resources and invocable. This method cannot
+         * be called more than once
+         *
+         * @tparam Args the non-handling argument types
+         * @tparam Func the invocable type
+         * @param functor the rvalue reference to the invocable object
+         * @param args the invocable's non-handling arguments
+         * @return the delegated functor
+         */
+        template <PureType... Args,
+            std::invocable<Handles..., Args...> Func>
+        [[nodiscard]] ArgumentsWrapper<Args...>::DelegatedFunctor<Func>
+            operator() (Func&& functor, Args... args);
+    private:
+        /**
+         * Converts the shared pointers to the weak pointers
+         */
+        class WeakPointers {
+            /**
+             * Returns the tuple containing the weak versions of the
+             * given shared pointers
+             *
+             * @tparam Args the shared pointers types
+             * @param handles the shared pointers
+             * @return the tuple used to determin weak pointers types
+             */
+            template <typename... Args>
+            static constexpr auto weaken(Args... handles);
+        public:
+            using Type = decltype(weaken(std::declval<Handles>()...));
+        };
+
+        typedef WeakPointers::Type              HandlingTuple;
+
+        HandlingTuple                           handles;
+    };
+
+}
+
+#include <MPGL/Utility/DelegationWrapper.tpp>
---
+++ include/MPGL/Utility/DelegationWrapper.tpp
@@ -0,0 +1,125 @@
+/**
+ *  MPGL - Modern and Precise Graphics Library
+ *
+ *  Copyright (c) 2021-2022
+ *      Grzegorz Czarnecki (grzegorz.czarnecki.2021@gmail.com)
+ *
+ *  This software is provided 'as-is', without any express or
+ *  implied warranty. In no event will the authors be held liable
+ *  for any damages arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any
+ *  purpose, including commercial applications, and to alter it and
+ *  redistribute it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented;
+ *  you must not claim that you wrote the original software.
+ *  If you use this software in a product, an acknowledgment in the
+ *  product documentation would be appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such,
+ *  and must not be misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source
+ *  distribution
+ */
+#pragma once
+
+namespace mpgl {
+
+    template <InstanceOf<std::shared_ptr>... Handles>
+    template <typename... Args>
+    constexpr auto
+        DelegationWrapper<Handles...>::WeakPointers::weaken(
+            Args... handles)
+    {
+        return std::tuple{std::weak_ptr{handles}...};
+    }
+
+    template <InstanceOf<std::shared_ptr>... Handles>
+    template <PureType... Args>
+    template <std::invocable<Handles..., Args...> Function>
+    DelegationWrapper<Handles...>::ArgumentsWrapper<Args...>::
+    DelegatedFunctor<Function>::DelegatedFunctor(
+        DelegationWrapper&& handles,
+        ArgumentsWrapper&& arguments,
+        Function&& invocable)
+            : handles{std::move(handles)},
+            arguments{std::move(arguments)},
+            invocable{std::move(invocable)} {}
+
+    template <InstanceOf<std::shared_ptr>... Handles>
+    template <PureType... Args>
+    template <std::invocable<Handles..., Args...> Function>
+    void DelegationWrapper<Handles...>::ArgumentsWrapper<Args...>::
+        DelegatedFunctor<Function>::operator() (void) const
+    {
+        if (hasExpired())
+            return;
+        std::apply(invocable,
+            std::tuple_cat(
+                replenishSharedPointers(),
+                arguments.args
+            )
+        );
+    }
+
+    template <InstanceOf<std::shared_ptr>... Handles>
+    template <PureType... Args>
+    template <std::invocable<Handles..., Args...> Function>
+    bool DelegationWrapper<Handles...>::ArgumentsWrapper<Args...>::
+        DelegatedFunctor<Function>::hasExpired(void) const
+    {
+        return std::apply([](auto&&... args) {
+            return (args.expired() && ...);
+        }, handles.handles);
+    }
+
+    template <InstanceOf<std::shared_ptr>... Handles>
+    template <PureType... Args>
+    template <std::invocable<Handles..., Args...> Function>
+    auto DelegationWrapper<Handles...>::ArgumentsWrapper<Args...>::
+        DelegatedFunctor<Function>::replenishSharedPointers(void) const
+    {
+        return std::apply([](auto&&... args) {
+            return std::tuple{args.lock()...};
+        }, handles.handles);
+    }
+
+    template <InstanceOf<std::shared_ptr>... Handles>
+    template <PureType... Args>
+    DelegationWrapper<Handles...>::ArgumentsWrapper<Args...>::
+        ArgumentsWrapper(DelegationWrapper&& wrapper, Args... args)
+            : wrapper{std::move(wrapper)}, args{args...} {}
+
+    template <InstanceOf<std::shared_ptr>... Handles>
+    template <PureType... Args>
+    template <std::invocable<Handles..., Args...> Function>
+    [[nodiscard]] DelegationWrapper<Handles...>::
+        ArgumentsWrapper<Args...>::DelegatedFunctor<Function>
+    DelegationWrapper<Handles...>::ArgumentsWrapper<Args...>::
+        operator() (Function&& func)
+    {
+        return DelegatedFunctor{std::move(wrapper),
+            std::move(*this), std::move(func)};
+    }
+
+    template <InstanceOf<std::shared_ptr>... Handles>
+    DelegationWrapper<Handles...>::DelegationWrapper(Handles... handles)
+        : handles{std::weak_ptr{handles}...} {}
+
+
+    template <InstanceOf<std::shared_ptr>... Handles>
+    template <PureType... Args,
+        std::invocable<Handles..., Args...> Func>
+    [[nodiscard]] DelegationWrapper<Handles...>::
+        ArgumentsWrapper<Args...>::DelegatedFunctor<Func>
+    DelegationWrapper<Handles...>::operator() (
+        Func&& functor,
+        Args... args)
+    {
+        return ArgumentsWrapper<Args...>{std::move(*this),
+            args...}(std::move(functor));
+    }
+
+}
