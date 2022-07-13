7a891a04527c851dcf860786be0cf7d2ba2800be
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2022-07-13 22:43:33 +0200

    Added comments to the basic window event manager class

--- include/MPGL/Events/Management/BasicWindowEventManager.hpp
+++ include/MPGL/Events/Management/BasicWindowEventManager.hpp
@@ -29,8 +29,15 @@
 
 namespace mpgl {
 
+    /**
+     * Calls all event observers directly after the occurrence of
+     * the event. Used by default in the MPGL windows
+     */
     class BasicWindowEventManager : public WindowEventManager {
     public:
+        /**
+         * Constructs a new basic window event manager object
+         */
         explicit BasicWindowEventManager(void) noexcept = default;
 
         BasicWindowEventManager(
@@ -43,29 +50,91 @@ namespace mpgl {
         BasicWindowEventManager& operator=(
             BasicWindowEventManager&&) = default;
 
+        /**
+         * Informs screen transformation event observers that the
+         * screen dimensions has been changed
+         *
+         * @param oldDimensions the old dimensions of the screen
+         */
         void onScreenTransformation(
             Vector2u const& oldDimensions) noexcept final;
 
+        /**
+         * Informs mouse release event observers that the mouse button
+         * has been released
+         *
+         * @param button the clicked mouse button
+         */
         void onMouseRelease(MouseButton const& button) noexcept final;
 
+        /**
+         * Informs window close event observers that window has been
+         * closed
+         */
         void onWindowClose(void) noexcept final;
 
+        /**
+         * Informs mouse motion event observers that the mouse has been
+         * moved
+         *
+         * @param position the new position of the mouse (in the
+         * coordinates relative to the window)
+         */
         void onMouseMotion(Vector2f const& position) noexcept final;
 
+        /**
+         * Informs mouse press event observers that the mouse button
+         * has been pressed
+         *
+         * @param button the clicked mouse button
+         */
         void onMousePress(MouseButton const& button) noexcept final;
 
+        /**
+         * Informs key release event observers that the keyboard key
+         * has been released
+         *
+         * @param key the released keyboard key
+         */
         void onKeyRelease(Key const& key) noexcept final;
 
+        /**
+         * Informs text write event observers that the text has been
+         * written
+         *
+         * @param unicodeString the written text in the unicode format
+         */
         void onTextWrite(
             std::string const& unicodeString) noexcept final;
 
+        /**
+         * Informs key press event observers that the keyboard key has
+         * been pressed
+         *
+         * @param key the pressed keyboard key
+         */
         void onKeyPress(Key const& key) noexcept final;
 
+        /**
+         * Informs scroll event observers that the mouse scroll has
+         * been scrolled
+         *
+         * @param scroll the mouse scroll values
+         */
         void onScroll(Vector2f const& scroll) noexcept final;
 
+        /**
+         * Informs tick event observers that the tick has occurred
+         *
+         * @param delta the time difference between last and current
+         * tick
+         */
         void onTick(
             std::chrono::milliseconds const& delta) noexcept final;
 
+        /**
+         * Destroys the basic window event manager object
+         */
         ~BasicWindowEventManager(void) noexcept = default;
     private:
         typedef std::shared_ptr<ScreenTransformationEvent>  STEPtr;
@@ -79,44 +148,164 @@ namespace mpgl {
         typedef std::shared_ptr<ScrollEvent>                SEPtr;
         typedef std::shared_ptr<TickEvent>                  TEPtr;
 
+        /**
+         * Registers screen transformation event pointer in the manager
+         *
+         * @param event the constant reference to the screen
+         * transformation event shared pointer
+         */
         void push(STEPtr const& event) final;
 
+        /**
+         * Registers screen transformation event pointer in the manager
+         *
+         * @param event the rvalue reference to the screen
+         * transformation event shared pointer
+         */
         void push(STEPtr&& event) final;
 
+        /**
+         * Registers mouse release event pointer in the manager
+         *
+         * @param event the constant reference to the mouse release
+         * event shared pointer
+         */
         void push(MREPtr const& event) final;
 
+        /**
+         * Registers mouse release event pointer in the manager
+         *
+         * @param event the rvalue reference to the mouse release
+         * event shared pointer
+         */
         void push(MREPtr&& event) final;
 
+        /**
+         * Registers mouse motion event pointer in the manager
+         *
+         * @param event the constant reference to the mouse motion
+         * event shared pointer
+         */
         void push(MMEPtr const& event) final;
 
+        /**
+         * Registers mouse motion event pointer in the manager
+         *
+         * @param event the rvalue reference to the mouse motion
+         * event shared pointer
+         */
         void push(MMEPtr&& event) final;
 
+        /**
+         * Registers window close event pointer in the manager
+         *
+         * @param event the constant reference to the window close
+         * event shared pointer
+         */
         void push(WCEPtr const& event) final;
 
+        /**
+         * Registers window close event pointer in the manager
+         *
+         * @param event the rvalue reference to the window close
+         * event shared pointer
+         */
         void push(WCEPtr&& event) final;
 
+        /**
+         * Registers mouse press event pointer in the manager
+         *
+         * @param event the constant reference to the mouse press
+         * event shared pointer
+         */
         void push(MPEPtr const& event) final;
 
+        /**
+         * Registers mouse press event pointer in the manager
+         *
+         * @param event the rvalue reference to the mouse press
+         * event shared pointer
+         */
         void push(MPEPtr&& event) final;
 
+        /**
+         * Registers keyboard release event pointer in the manager
+         *
+         * @param event the constant reference to the keyboard release
+         * event shared pointer
+         */
         void push(KREPtr const& event) final;
 
+        /**
+         * Registers keyboard release event pointer in the manager
+         *
+         * @param event the rvalue reference to the keyboard release
+         * event shared pointer
+         */
         void push(KREPtr&& event) final;
 
+        /**
+         * Registers text write event pointer in the manager
+         *
+         * @param event the constant reference to the text write
+         * event shared pointer
+         */
         void push(TWEPtr const& event) final;
 
+        /**
+         * Registers text write event pointer in the manager
+         *
+         * @param event the rvalue reference to the text write
+         * event shared pointer
+         */
         void push(TWEPtr&& event) final;
 
+        /**
+         * Registers key press event pointer in the manager
+         *
+         * @param event the constant reference to the keyboard press
+         * event shared pointer
+         */
         void push(KPEPtr const& event) final;
 
+        /**
+         * Registers key press event pointer in the manager
+         *
+         * @param event the constant reference to the keyboard press
+         * event shared pointer
+         */
         void push(KPEPtr&& event) final;
 
+        /**
+         * Registers scroll event pointer in the manager
+         *
+         * @param event the constant reference to the scroll event
+         * shared pointer
+         */
         void push(SEPtr const& event) final;
 
+        /**
+         * Registers rvalue event pointer in the manager
+         *
+         * @param event the constant reference to the scroll event
+         * shared pointer
+         */
         void push(SEPtr&& event) final;
 
+        /**
+         * Registers tick event pointer in the manager
+         *
+         * @param event the constant reference to the tick event
+         * shared pointer
+         */
         void push(TEPtr const& event) final;
 
+        /**
+         * Registers tick event pointer in the manager
+         *
+         * @param event the rvalue reference to the tick event
+         * shared pointer
+         */
         void push(TEPtr&& event) final;
 
         EventBus                                            registers;
