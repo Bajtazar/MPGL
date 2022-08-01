cd26caec8b08b80a51ba0c47397bf7b19e8d6615
Bajtazar <wahadlowiec@interia.pl>
2022-08-02 00:17:43 +0200

    Added index sequence to the traits

---
+++ include/MPGL/Traits/IndexSequence.hpp
@@ -0,0 +1,94 @@
+/**
+ *  MPGL - Modern and Precise Graphics Library
+ *
+ *  Copyright (c) 2021-2022
+ *      Grzegorz Czarnecki (grzegorz.czarnecki.2021@gmail.com)
+ *
+ *  This software is provided 'as-is', without any express or
+ *  implied warranty. In no event will the authors be held liable
+ *  for any damages arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any
+ *  purpose, including commercial applications, and to alter it and
+ *  redistribute it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented;
+ *  you must not claim that you wrote the original software.
+ *  If you use this software in a product, an acknowledgment in the
+ *  product documentation would be appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such,
+ *  and must not be misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source
+ *  distribution
+ */
+#pragma once
+
+#include <MPGL/Traits/Concepts.hpp>
+
+namespace mpgl {
+
+    /**
+     * Adds the given number to the front of index sequence
+     *
+     * @tparam I the inserted number
+     * @tparam Indexes the index sequence's indexes
+     * @return the index sequence with inserted number
+     */
+    template <size_t I, size_t... Indexes>
+    [[nodiscard]] consteval std::index_sequence<Indexes..., I>
+        addToIndexSequence(std::index_sequence<Indexes...>) noexcept;
+
+    /**
+     * Produces index sequence that holds only indexes between
+     * the Begin index and the End index (excluding End)
+     *
+     * @tparam Index the current index [begin or continuation of
+     * recursive call]
+     * @tparam End the end index
+     * @tparam Indexes the current index sequence's indexes
+     * @param indexes the current index sequence
+     * @return the range index sequence
+     */
+    template <size_t Index, size_t End, size_t... Indexes>
+    [[nodiscard]] consteval decltype(auto) indexSequenceRange(
+        std::index_sequence<Indexes...> indexes) noexcept;
+
+    /**
+     * Produces index sequence that holds only indexes between
+     * the Begin index and the End index (excluding End)
+     *
+     * @tparam Begin first index
+     * @tparam End first index after last
+     */
+    template <size_t Begin, size_t End>
+    using makeIndexSequenceRange = decltype(
+        indexSequenceRange<Begin + 1, End>(
+            std::index_sequence<Begin>{}));
+
+    /**
+     * Splits sequence between the ranges and returns a tuple
+     * containing them
+     *
+     * @tparam Indexes the index sequence's indexes
+     * @param indexes the index sequence
+     * @return a tuple containing subranges
+     */
+    template <size_t... Indexes>
+    [[nodiscard]] consteval decltype(auto) splitIndexSequence(
+        std::index_sequence<Indexes...> indexes) noexcept;
+
+    /**
+     * Splits sequence between the ranges and returns a tuple
+     * containing them
+     */
+    template <class IndexSequence>
+        requires requires(IndexSequence seq) {
+            splitIndexSequence(seq); }
+    using SplitedIndexSequence = decltype(
+        splitIndexSequence(std::declval<IndexSequence>()));
+
+}
+
+#include <MPGL/Traits/IndexSequence.tpp>
