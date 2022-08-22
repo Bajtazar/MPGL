20cebbabc0f2811663d2dc888bbcef8bdf9916d4
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2022-08-22 15:32:11 +0200

    Added indicies triangle

---
+++ include/MPGL/Core/Vertex/Indicies/IndiciesTriangle.hpp
@@ -0,0 +1,46 @@
+/**
+ *  MPGL - Modern and Precise Graphics Library
+ *
+ *  Copyright (c) 2021-2022
+ *      Grzegorz Czarnecki (grzegorz.czarnecki.2021@gmail.com)
+ *
+ *  This software is provided 'as-is', without any express or
+ *  implied warranty. In no event will the authors be held liable
+ *  for any damages arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any
+ *  purpose, including commercial applications, and to alter it and
+ *  redistribute it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented;
+ *  you must not claim that you wrote the original software.
+ *  If you use this software in a product, an acknowledgment in the
+ *  product documentation would be appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such,
+ *  and must not be misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source
+ *  distribution
+ */
+#pragma once
+
+#include <MPGL/Traits/Types.hpp>
+
+namespace mpgl {
+
+    #pragma pack(push, 1)
+
+    /**
+     * Constains informations about vertices that are used
+     * to draw the given triangle
+     */
+    struct IndiciesTriangle {
+        uint32                              firstVertex;
+        uint32                              secondVertex;
+        uint32                              thirdVertex;
+    };
+
+    #pragma pack(pop)
+
+}
