30b3514be3c82b3b3b4e8c642783cdb249dc0b02
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2022-10-11 20:50:57 +0200

    Added comments to column view

--- include/MPGL/Mathematics/Tensors/Views.hpp
+++ include/MPGL/Mathematics/Tensors/Views.hpp
@@ -31,12 +31,24 @@ namespace mpgl {
 
     namespace details {
 
+        /**
+         * Checks whether the given type is a specialization
+         * of the matrix template class or if it is its view
+         *
+         * @tparam Tp the checked type
+         */
         template <class Tp>
         concept MatrixView = MatrixType<Tp> || MatrixType<decltype(
             std::declval<Tp>().base())>;
 
     }
 
+    /**
+     * Views a column of a matrx
+     *
+     * @tparam MatrixTp the matrix specialization type or its
+     * view type
+     */
     template <details::MatrixView MatrixTp>
     class ColumnView : public std::ranges::view_interface<
         ColumnView<MatrixTp>>
@@ -51,16 +63,39 @@ namespace mpgl {
         using vector_form = Vector<
             std::remove_cv_t<value_type>, matrix_type::width()>;
 
+        /**
+         * Constructs a new column view object
+         *
+         * @param matrix an universal reference to the view
+         * of the matrix specialization
+         * @param columnID an index of the viewed column
+         */
         constexpr explicit ColumnView(
             MatrixTp&& matrix,
             std::size_t columnID) noexcept;
 
+        /**
+         * Assigns a vector with the same layout to this
+         * column
+         *
+         * @param vector a constant reference to the assigned
+         * vector
+         * @return a reference to this object
+         */
         constexpr ColumnView& operator=(
             vector_form const& vector) noexcept;
 
+        /**
+         * Casts viewed column into its vector form
+         *
+         * @return the column in its vector form
+         */
         [[nodiscard]] constexpr operator
             vector_form() const noexcept;
 
+        /**
+         * Iterates over the elements of the matrix's column
+         */
         class iterator {
         public:
             using reference = decltype(
@@ -70,65 +105,170 @@ namespace mpgl {
             using difference_type = std::ptrdiff_t;
             using iterator_category = std::random_access_iterator_tag;
 
+            /**
+             * Constructs a new iterator object
+             */
             constexpr explicit iterator(void) noexcept = default;
 
+            /**
+             * Constructs a new iterator object
+             *
+             * @param matrix an universal reference to the view
+             * of the matrix specialization
+             * @param columnID an index of the viewed column
+             * @param rowID an index of the first viewed row
+             */
             constexpr explicit iterator(
                 MatrixTp&& matrix,
                 std::size_t columnID,
                 std::size_t rowID = 0) noexcept;
 
+            /**
+             * Increments iterator by one
+             *
+             * @return reference to this object
+             */
             constexpr iterator& operator++(void) noexcept;
 
+            /**
+             * Post-increments iterator by one and returns copy
+             * of the object
+             *
+             * @return the copied object
+             */
             [[nodiscard]] constexpr iterator operator++(
                 int) noexcept;
 
+            /**
+             * Decrements iterator by one
+             *
+             * @return reference to this object
+             */
             constexpr iterator& operator--(void) noexcept;
 
+            /**
+             * Post-decrements iterator by one and returns copy
+             * of the object
+             *
+             * @return the copied object
+             */
             [[nodiscard]] constexpr iterator operator--(
                 int) noexcept;
 
+            /**
+             * Returns a reference to the matrix's column element
+             *
+             * @return reference to the matrix's column element
+             */
             [[nodiscard]] constexpr reference operator*(
                 void) const noexcept;
 
+            /**
+             * Returns a pointer to the matrix's column element
+             *
+             * @return pointer to the matrix's column element
+             */
             [[nodiscard]] constexpr pointer operator->(
                 void) const noexcept;
 
+            /**
+             * Increments iterator by the given distance
+             *
+             * @param offset the incremented distance
+             * @return reference to this object
+             */
             constexpr iterator& operator+=(difference_type offset
                 ) noexcept;
 
+            /**
+             * Decrements iterator by the given distance
+             *
+             * @param offset the decremented distance
+             * @return reference to this object
+             */
             constexpr iterator& operator-=(difference_type offset
                 ) noexcept;
 
+            /**
+             * Returns a reference to the element shifted by
+             * the given offset
+             *
+             * @param offset the shift distance
+             * @return the reference to the element shifted by
+             * the given offset
+             */
             [[nodiscard]] constexpr reference
                 operator[](difference_type offset
                     ) const noexcept;
 
+            /**
+             * Adds given distance to an iterator
+             *
+             * @param left the iterator
+             * @param right the distance
+             * @return the shifted iterator
+             */
             [[nodiscard]] friend constexpr iterator operator+ (
                 iterator const& left,
                 difference_type right) noexcept
                     { return iterator{left.ownerPtr, left.rowId + right}; }
 
+            /**
+             * Adds given distance to an iterator
+             *
+             * @param left the distance
+             * @param right the iterator
+             * @return the shifted iterator
+             */
             [[nodiscard]] friend constexpr iterator operator+ (
                 difference_type left,
                 iterator const& right) noexcept
                     { return iterator{right.ownerPtr, right.rowId + left}; }
 
+            /**
+             * Subtracts given distance from iterator
+             *
+             * @param left the iterator
+             * @param right the distance
+             * @return the shifted operator
+             */
             [[nodiscard]] friend constexpr iterator operator- (
                 iterator const& left,
                 difference_type right) noexcept
                     { return iterator{left.ownerPtr, left.rowId - right}; }
 
+            /**
+             * Returns distance between iterators
+             *
+             * @param left the left iterator
+             * @param right the right iterator
+             * @return difference_type
+             */
             [[nodiscard]] friend constexpr difference_type
                 operator- (
                     iterator const& left,
                     iterator const& right) noexcept
                         { return (difference_type)left.rowID - right.rowID; }
 
+            /**
+             * Compares two iterators to each other
+             *
+             * @param left the left iterator
+             * @param right the right iterator
+             * @return the result of compare
+             */
             [[nodiscard]] friend constexpr auto operator<=> (
                 iterator const& left,
                 iterator const& right) noexcept
                     { return left.rowID <=> right.rowID; }
 
+            /**
+             * Checks whether two iterators are equal
+             *
+             * @param left the left iterator
+             * @param right the right iterator
+             * @return whether two iterators are equal
+             */
             [[nodiscard]] friend constexpr bool operator== (
                 iterator const& left,
                 iterator const& right) noexcept
@@ -141,55 +281,159 @@ namespace mpgl {
 
         using reverse_iterator = std::reverse_iterator<iterator>;
 
+        /**
+         * Returns the reference to element with the given index
+         *
+         * @param index the element's index
+         * @return the reference to element with the given index
+         */
         [[nodiscard]] constexpr reference operator[] (
             std::size_t index) const noexcept;
 
+        /**
+         * Adds the elements of this column with
+         * the given column elements
+         *
+         * @param right the added column
+         * @return the reference to this column
+         */
         constexpr ColumnView& operator+=(
             ColumnView const& right) noexcept;
 
+        /**
+         * Subtracts the elements of this column with
+         * the given column elements
+         *
+         * @param right the subtracted column
+         * @return the reference to this column
+         */
         constexpr ColumnView& operator-=(
             ColumnView const& right) noexcept;
 
+       /**
+         * Multiplies the elements of this column with
+         * the given column elements
+         *
+         * @param right the multiplicand column
+         * @return the reference to this column
+         */
         constexpr ColumnView& operator*=(
             ColumnView const& right) noexcept;
 
+        /**
+         * Divides the elements of this column by the
+         * given column elements
+         *
+         * @param right the divider column
+         * @return the reference to this column
+         */
         constexpr ColumnView& operator/=(
             ColumnView const& right) noexcept;
 
+        /**
+         * Adds the given scalar value to the elements of
+         * this column
+         *
+         * @param right the added scalar
+         * @return the reference to this column
+         */
         constexpr ColumnView& operator+=(
             value_type const& right) noexcept;
 
+        /**
+         * Subtracts the given scalar value to the elements of
+         * this column
+         *
+         * @param right the subtracted scalar
+         * @return the reference to this column
+         */
         constexpr ColumnView& operator-=(
             value_type const& right) noexcept;
 
+        /**
+         * Multiplies the given scalar column to the elements of
+         * this column
+         *
+         * @param right the multiplied scalar
+         * @return the reference to this column
+         */
         constexpr ColumnView& operator*=(
             value_type const& right) noexcept;
 
+        /**
+         * Divides the elements of this column by the given
+         * scalar value
+         *
+         * @param right the divisor scalar
+         * @return the reference to this column
+         */
         constexpr ColumnView& operator/=(
             value_type const& right) noexcept;
 
-        [[nodiscard]] constexpr MatrixTp const& base(void) const &
-            { return matrix; }
+        /**
+         * Returns a constant reference to the underlying
+         * matrix specialization's view
+         *
+         * @return the constant reference to the underlying
+         * matrix specialization's view
+         */
+        [[nodiscard]] constexpr MatrixTp const& base(
+            void) const & noexcept
+                { return matrix; }
 
-        [[nodiscard]] constexpr MatrixTp&& base(void) &&
+        /**
+         * Returns a rvalue reference to the underlying
+         * matrix specialization's view
+         *
+         * @return the rvalue reference to the underlying
+         * matrix specialization's view
+         */
+        [[nodiscard]] constexpr MatrixTp&& base(void) && noexcept
             { return std::move(matrix); }
 
+        /**
+         * Returns an iterator to the begining of the column
+         *
+         * @return the iterator to the begining of the column
+         */
         [[nodiscard]] constexpr iterator begin(
             void) const noexcept
                 { return iterator{ matrix.base(), columnID }; }
 
+        /**
+         * Returns an iterator to the end of the column
+         *
+         * @return the iterator to the end of the column
+         */
         [[nodiscard]] constexpr iterator end(
             void) const noexcept
                 { return iterator{ matrix.base(), columnID, matrix.base().size() }; }
 
+        /**
+         * Returns a reverse iterator to the end of the column
+         *
+         * @return the reverse iterator to the end of the column
+         */
         [[nodiscard]] constexpr reverse_iterator
             rbegin(void) const noexcept
                 { return reverse_iterator{ end() - 1 }; }
 
+        /**
+         * Returns a reverse iterator to the begining of
+         * the column
+         *
+         * @return the reverse iterator to the begining of
+         * the column
+         */
         [[nodiscard]] constexpr reverse_iterator
             rend(void) const noexcept
                 { return reverse_iterator{ begin() - 1 }; }
 
+        /**
+         * Returns the size of the column view
+         *
+         * @return the size of the column
+         */
         [[nodiscard]] static constexpr auto size(
             void) noexcept
                 { return matrix_type::size(); }
