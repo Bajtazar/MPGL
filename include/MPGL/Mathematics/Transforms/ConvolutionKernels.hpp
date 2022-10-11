1d0b297269d056673463ac972b025b729e88c7f2
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2022-10-11 18:55:32 +0200

    Made all convolution kernels inlined

--- include/MPGL/Mathematics/Transforms/ConvolutionKernels.hpp
+++ include/MPGL/Mathematics/Transforms/ConvolutionKernels.hpp
@@ -32,35 +32,35 @@ namespace mpgl {
     namespace kernels {
 
         /// The gaussian blur
-        constexpr const Matrix3f gaussian {
+        inline constexpr const Matrix3f gaussian {
             Vector3f{0.0625f, 0.125f, 0.0625},
             Vector3f{0.125f, 0.25f, 0.125f},
             Vector3f{0.0625f, 0.125f, 0.0625}
         };
 
         /// The mean filter
-        constexpr const Matrix3f mean {
+        inline constexpr const Matrix3f mean {
             Vector3f{1.f / 9.f, 1.f / 9.f, 1.f / 9.f},
             Vector3f{1.f / 9.f, 1.f / 9.f, 1.f / 9.f},
             Vector3f{1.f / 9.f, 1.f / 9.f, 1.f / 9.f}
         };
 
         /// The laplacian filter
-        constexpr const Matrix3f laplacian {
+        inline constexpr const Matrix3f laplacian {
             Vector3f{0.f, -1.f, 0.f},
             Vector3f{-1.f, 4.f, -1.f},
             Vector3f{0.f, -1.f, 0.f}
         };
 
         /// The embos filter
-        constexpr const Matrix3f embos {
+        inline constexpr const Matrix3f embos {
             Vector3f{2.f, 0.f, 0.f},
             Vector3f{0.f, -1.f, 0.f},
             Vector3f{0.f, 0.f, -1.f}
         };
 
         /// The sharpness filter
-        constexpr const Matrix3f sharpness {
+        inline constexpr const Matrix3f sharpness {
             Vector3f{-1.f, -1.f, -1.f},
             Vector3f{-1.f,  9.f, -1.f},
             Vector3f{-1.f, -1.f, -1.f}
